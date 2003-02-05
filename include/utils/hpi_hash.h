742a94aceb9f989c998e1f504214242db6b9b828
abrugger1701 <abrugger1701@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2003-02-05 00:38:26 +0000

    created an hpi_macros header file

--- include/utils/hpi_hash.h
+++ include/utils/hpi_hash.h
@@ -9,17 +9,10 @@
 #ifndef HASH_H
 #define HASH_H
 
+#include <hpi_macros.h>
 
 #define HASH_TABLE_MIN_SIZE 11
 #define HASH_TABLE_MAX_SIZE 13845163
-
-#define TRUE 1
-#define FALSE 0
-
-typedef int (*EqualFunc) (const void *a, const void *b);
-typedef void (*DestroyNotify) (void *data);
-typedef unsigned int (*HashFunc) (const void *key);
-typedef void (*FreeFunc) (void *data);
 
 typedef struct _HashTable HashTable;
 typedef struct _HashNode HashNode;
@@ -37,26 +30,5 @@ void 			hash_table_insert(HashTable *hash_table,
 int			hash_table_steal(HashTable *hash_table,
 					 const void *key);
 
-#undef  CLAMP
-#define CLAMP(x, low, high)  (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
-
-
-
-extern void glib_dummy_decl (void);
-#define LOCK_NAME(name)               g__ ## name ## _lock
-#ifdef  THREADS_ENABLED
-#if 0
-#    define LOCK(name) g_static_mutex_lock       (&LOCK_NAME (name))
-#    define UNLOCK(name) g_static_mutex_unlock   (&LOCK_NAME (name))
-#    define TRYLOCK(name) g_static_mutex_trylock (&_LOCK_NAME (name))
-#endif
-#else   /* !THREADS_ENABLED */
-#  define LOCK_DEFINE_STATIC(name)    extern void glib_dummy_decl (void)
-#  define LOCK_DEFINE(name)           extern void glib_dummy_decl (void)
-#  define LOCK_EXTERN(name)           extern void glib_dummy_decl (void)
-#  define LOCK(name)
-#  define UNLOCK(name)
-#  define TRYLOCK(name)               (TRUE)
-#endif  /* !THREADS_ENABLED */
 
 #endif /* HASH_H */
