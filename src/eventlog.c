788f2568ea03c0a9a454e7d13c2b1ca103498f70
yzhuang <yzhuang@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2003-07-04 05:41:50 +0000

    * add event log. domain event log is OK, but resource event log needs redesign in event log interface

--- src/eventlog.c
+++ src/eventlog.c
@@ -24,35 +24,145 @@
 
 int dsel_get_info(SaHpiDomainIdT domain_id, SaHpiSelInfoT *info)
 {
+	struct oh_domain *d;
+	
+	d = get_domain_by_id(domain_id);
+	if (!d) {
+		dbg("Invalid domain");
+		return -1;
+	}
+	
+	info->Entries			= g_slist_length(d->sel);
+	info->Size			= 0xFFFFFFFF;
+	info->UpdateTimestamp		= 0;
+	gettimeofday1(&info->CurrentTime);
+	info->Enabled	= (d->sel_state==OH_SEL_ENABLED) ? 1 : 0;
+	info->OverflowFlag		= 0;
+	info->OverflowAction		= SAHPI_SEL_OVERFLOW_DROP;
+	info->DeleteEntrySupported 	= 1;
+
 	return 0;
 }
 
 int dsel_get_state(SaHpiDomainIdT domain_id)
 {
-	return 0;
+	struct oh_domain *d;
+	
+	d = get_domain_by_id(domain_id);
+	if (!d) {
+		dbg("Invalid domain");
+		return -1;
+	}
+	
+	return (d->sel_state==OH_SEL_ENABLED) ? 1 : 0;
 }
 
 void dsel_set_state(SaHpiDomainIdT domain_id, int enable)
 {
+	struct oh_domain *d;
+	
+	d = get_domain_by_id(domain_id);
+	if (!d) {
+		dbg("Invalid domain");
+		return;
+	}
+	
+	d->sel_state = enable ? OH_SEL_ENABLED : OH_SEL_DISABLED;
 }
 
 SaHpiTimeT dsel_get_time(SaHpiDomainIdT domain_id)
 {
-	return 0;
+	SaHpiTimeT cur;
+	gettimeofday1(&cur);
+	return cur;
 }
 
 void dsel_set_time(SaHpiDomainIdT domain_id, SaHpiTimeT time)
 {
+	/* just escape */
+	return; 
 }
 
 void dsel_add(SaHpiDomainIdT domain_id, SaHpiSelEntryT *entry)
 {
+	struct oh_domain *d;
+	struct oh_sel *sel;
+	
+	d = get_domain_by_id(domain_id);
+	if (!d) {
+		dbg("Invalid domain");
+		return;
+	}
+	
+	sel = malloc(sizeof(*sel));
+	if (!sel) {
+		dbg("Out of memory");
+		return;
+	}
+	memset(sel, 0, sizeof(*sel));
+
+	memcpy(&sel->entry, entry, sizeof(*entry));
+	sel->entry.EntryId = d->sel_counter++;
+	d->sel = g_slist_append(d->sel, sel);
+}
+
+void dsel_add2(struct oh_domain *d, struct oh_event *e)
+{
+	struct oh_sel *sel;
+	
+	sel = malloc(sizeof(*sel));
+	if (!sel) {
+		dbg("Out of memory");
+		return;
+	}
+	memset(sel, 0, sizeof(*sel));
+
+	sel->res_id		= e->u.hpi_event.parent;
+	sel->rdr_id		= e->u.hpi_event.id;
+	sel->entry.EntryId	= d->sel_counter++;
+	gettimeofday1(&sel->entry.Timestamp);
+	
+	memcpy(&sel->entry.Event, &e->u.hpi_event.event, sizeof(sel->entry.Event));
+	
+	d->sel = g_slist_append(d->sel, sel);
 }
 
 void dsel_del(SaHpiDomainIdT domain_id, SaHpiSelEntryIdT id)
 {
+	struct oh_domain *d;
+	GSList *i;
+	d = get_domain_by_id(domain_id);
+	if (!d) {
+		dbg("Invalid domain");
+		return;
+	}
+
+	g_slist_for_each(i, d->sel) {
+		struct oh_sel *sel;
+		sel = i->data;
+		if (sel->entry.EntryId == id) {
+			d->sel = g_slist_remove_link(d->sel, i);
+			free(sel);
+			break;
+		}
+	}
 }
 
 void dsel_clr(SaHpiDomainIdT domain_id) 
 {
+	
+	struct oh_domain *d;
+	GSList *i, *i2;
+	d = get_domain_by_id(domain_id);
+	if (!d) {
+		dbg("Invalid domain");
+		return;
+	}
+
+	g_slist_for_each_safe(i, i2, d->sel) {
+		struct oh_sel *sel;
+		sel = i->data;
+		d->sel = g_slist_remove_link(d->sel, i);
+		free(sel);			
+	}
 }
