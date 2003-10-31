65408c71794510cf39267d82dfc76d49d2b48322
sdague <sdague@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2003-10-31 21:06:13 +0000

    move these functions to a different file for now.  They aren't built into the system at this point

--- openhpi/branches/experimental/src/safhpi.c
+++ openhpi/branches/experimental/src/safhpi.c
@@ -415,16 +415,17 @@ SaErrorT SAHPI_API saHpiEntitySchemaGet(
         return SA_OK;
 }
 
-#if 0
 
 SaErrorT SAHPI_API saHpiEventLogInfoGet (
                 SAHPI_IN SaHpiSessionIdT SessionId,
                 SAHPI_IN SaHpiResourceIdT ResourceId,
                 SAHPI_OUT SaHpiSelInfoT *Info)
 {
+        RPTable *rpt = default_rpt;
+        SaHpiRptEntryT *res;
+        struct oh_handler *h;
         struct oh_session *s;
-        struct oh_resource *res;
-
+        
         OH_STATE_READY_CHECK;
         
         s = session_get(SessionId);
@@ -433,183 +434,37 @@ SaErrorT SAHPI_API saHpiEventLogInfoGet (
                 return SA_ERR_HPI_INVALID_SESSION;
         }       
         
+        /*
+          Waiting for the right way to do DSELS 
+          
         if (ResourceId==SAHPI_DOMAIN_CONTROLLER_ID) {
                 if(dsel_get_info(s->domain_id, Info)<0)
                         return SA_ERR_HPI_UNKNOWN;
                 return SA_OK;
         }
+        */
         
-        res = get_resource(ResourceId);
+        h = oh_get_resource_data(rpt, ResourceId);
+        if(!h) {
+                dbg("Can't find handler for ResourceId %d",ResourceId);
+                return SA_ERR_HPI_INVALID_RESOURCE;
+        }
+        
+        res = oh_get_resource_by_id(rpt, ResourceId);
         if (!res) {
                 dbg("Invalid resource");
                 return SA_ERR_HPI_INVALID_RESOURCE;
         }                                               
         
-        if (res->handler->abi->get_sel_info(res->handler->hnd, res->oid, Info)<0)
+        if(!(res->ResourceCapabilities & SAHPI_CAPABILITY_SEL)) {
+                dbg("Can't use SEL functions on this resource");
+                return SA_ERR_HPI_INVALID_REQUEST;
+        }
+
+        if (h->abi->get_sel_info(h->hnd, ResourceId, Info)<0)
                 return SA_ERR_HPI_UNKNOWN;
         return SA_OK;
 }
-
-struct list_entry_ops {
-        void                    (*get_entry)(void *, SaHpiSelEntryT *);
-        struct oh_resource      *(*get_res)(void *);
-        struct oh_rdr           *(*get_rdr)(void *);
-};
-
-static SaErrorT process_sel_entry(
-                GSList *sel_list, 
-                SaHpiSelEntryIdT EntryId,
-                SaHpiSelEntryIdT *PrevEntryId,
-                SaHpiSelEntryIdT *NextEntryId,
-                SaHpiSelEntryT *EventLogEntry,
-                SaHpiRdrT *Rdr,
-                SaHpiRptEntryT *RptEntry,
-                struct list_entry_ops *ops)
-{
-        struct oh_resource *res;
-        struct oh_rdr *rdr;
-        GSList *pi, *i, *ni;
-        SaHpiSelEntryT entry;
-
-        data_access_lock();
-
-        if (sel_list==NULL) {
-                data_access_unlock();
-                return SA_ERR_HPI_INVALID;
-        } else if (EntryId==SAHPI_OLDEST_ENTRY) {
-                pi = NULL;
-                i  = g_slist_nth(sel_list, 0);
-                ni = g_slist_nth(sel_list, 1);
-        } else if (EntryId==SAHPI_NEWEST_ENTRY) {
-                int num = g_slist_length(sel_list);
-                pi  = g_slist_nth(sel_list, num-2);
-                i   = g_slist_nth(sel_list, num-1);
-                ni  = NULL;
-        } else {
-                int num = EntryId;
-                pi = g_slist_nth(sel_list, num-1);
-                i  = g_slist_nth(sel_list, num);
-                ni = g_slist_nth(sel_list, num+1);
-        }
-        
-        if (!i) {
-                data_access_unlock();
-                return SA_ERR_HPI_INVALID;
-        }
-        
-        ops->get_entry(i->data, EventLogEntry);
-        EventLogEntry->EntryId =  EntryId;
-        res = ops->get_res(i->data);
-        
-        if (RptEntry) {
-                if (res) 
-                        memcpy(RptEntry, &res->entry, sizeof(*RptEntry));
-                else
-                        RptEntry->ResourceCapabilities = 0;
-        }
-
-        if (Rdr) {
-                if (res) 
-                        rdr = ops->get_rdr(i->data);
-                else 
-                        rdr = NULL;
-                if (rdr)
-                        memcpy(Rdr, &rdr->rdr, sizeof(*Rdr));
-                else 
-                        Rdr->RdrType = SAHPI_NO_RECORD;
-        }
-        
-        if (pi) {
-                ops->get_entry(pi->data, &entry);
-                *PrevEntryId = entry.EntryId;
-        } else 
-                *PrevEntryId = SAHPI_NO_MORE_ENTRIES;
-
-        if (ni) {
-                ops->get_entry(ni->data, &entry);
-                *NextEntryId = entry.EntryId;
-        } else
-                *NextEntryId = SAHPI_NO_MORE_ENTRIES;
-
-        data_access_unlock();
-        
-        return SA_OK;
-}
-
-static void dsel_get_entry(void *ptr, SaHpiSelEntryT *entry)
-{
-        struct oh_dsel *dsel = ptr;
-        memcpy(entry, &dsel->entry, sizeof(*entry));
-}
-
-static struct oh_resource *dsel_get_res(void *ptr)
-{
-        struct oh_dsel *dsel = ptr;
-        return get_res_by_oid(dsel->res_id);
-}
-
-static struct oh_rdr *dsel_get_rdr(void *ptr)
-{
-        struct oh_dsel *dsel = ptr;
-        struct oh_resource *res;
-
-        res = get_res_by_oid(dsel->res_id);
-        if (!res) {
-                dbg("Cannot find resource");
-                return NULL;
-        }
-
-        return get_rdr_by_oid(res, dsel->rdr_id);
-}
-
-static struct list_entry_ops dsel_ops = {
-        .get_entry = dsel_get_entry,
-        .get_res   = dsel_get_res,
-        .get_rdr   = dsel_get_rdr
-};
-
-static void rsel_get_entry(void *ptr, 
-                SaHpiSelEntryT *entry)
-{
-        struct oh_rsel *rsel = ptr;
-        struct oh_resource *res;
-        res = get_res_by_oid(rsel->parent);
-        if (!res) {
-                dbg("Cannot find resource");
-                return;
-        }
-
-        res->handler->abi->get_sel_entry(
-                        res->handler->hnd,
-                        rsel->oid, entry);
-        entry->EntryId = rsel->entry_id;
-}
-
-static struct oh_resource *rsel_get_res(void *ptr)
-{
-        struct oh_rsel *rsel = ptr;
-        return get_res_by_oid(rsel->res_id);
-}
-
-static struct oh_rdr *rsel_get_rdr(void *ptr)
-{
-        struct oh_rsel *rsel = ptr;
-        struct oh_resource *res;
-
-        res = get_res_by_oid(rsel->res_id);
-        if (!res) {
-                dbg("Cannot find resource");
-                return NULL;
-        }
-
-        return get_rdr_by_oid(res, rsel->rdr_id);
-}
-
-static struct list_entry_ops rsel_ops = {
-        .get_entry = rsel_get_entry,
-        .get_res   = rsel_get_res,
-        .get_rdr   = rsel_get_rdr
-};
 
 SaErrorT SAHPI_API saHpiEventLogEntryGet (
                 SAHPI_IN SaHpiSessionIdT SessionId,
