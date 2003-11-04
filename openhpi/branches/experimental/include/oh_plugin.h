4b44edcffbc07c872f917989e457228f75e062db
sdague <sdague@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2003-11-04 00:20:17 +0000

    change back to void * until dummy.c migrates data

--- openhpi/branches/experimental/include/oh_plugin.h
+++ openhpi/branches/experimental/include/oh_plugin.h
@@ -187,9 +187,9 @@ struct oh_abi_v1 {
 	 * @param addr the interface name.
 	 * for example, "ipaddr:port" for SNMP, "if_num" for IPMI SMI
 	 */
-	struct oh_handler_state *(*open)(GHashTable *handler_config);
+	void *(*open)(GHashTable *handler_config);
 	
-	void (*close)(struct oh_handler_state *hnd);
+	void (*close)(void *hnd);
 	/**
 	 * The function wait for event. 
 	 * 
