c896028083de09dd2aff7ffa895d5bb29bb2178e
sesherman <sesherman@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2003-11-04 17:04:00 +0000

    Add list controls

--- openhpi/trunk/examples/list_resources.c
+++ openhpi/trunk/examples/list_resources.c
@@ -1,3 +1,6 @@
+/*      -*- linux-c -*-
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <SaHpi.h>
@@ -17,10 +20,13 @@ const char * severity2str(SaHpiSeverityT);
 const char * rdrtype2str(SaHpiRdrTypeT type);
 char * interpreted2str (SaHpiSensorInterpretedT interpreted);
 char * rpt_cap2str(SaHpiCapabilitiesT ResourceCapabilities);
+const char * ctrldigital2str(SaHpiCtrlStateDigitalT digstate);
 const char * get_sensor_type(SaHpiSensorTypeT type);
+const char * get_control_type(SaHpiCtrlTypeT type);
 const char * get_sensor_category(SaHpiEventCategoryT category);
 void list_rdr(SaHpiSessionIdT session_id, SaHpiResourceIdT resource_id);
-void display_id_string(SaHpiTextBufferT string);
+void display_textbuffer(SaHpiTextBufferT string);
+void display_oembuffer(SaHpiUint32T length, SaHpiUint8T *string);
 void printreading (SaHpiSensorReadingT reading);
 
 /**
@@ -114,6 +120,13 @@ SaErrorT discover_domain(SaHpiDomainIdT domain_id, SaHpiSessionIdT session_id, S
                 printf("Domain ID: %x\n", (int) entry.DomainId);
                 printf("Revision: %c\n", entry.ResourceInfo.ResourceRev);
                 printf("Version: %c\n", entry.ResourceInfo.SpecificVer);
+		printf("Device Support: %c\n", entry.ResourceInfo.DeviceSupport);
+		printf("Manufacturer ID: %x\n", (int) entry.ResourceInfo.ManufacturerId);
+		printf("Product ID: %x\n", (int) entry.ResourceInfo.ProductId);
+		printf("Firmware Major, Minor, Aux: %c %c %c\n", 
+		       entry.ResourceInfo.FirmwareMajorRev, 
+		       entry.ResourceInfo.FirmwareMinorRev,
+		       entry.ResourceInfo.AuxFirmwareRev);
                 printf("Severity: %s\n",severity2str(entry.ResourceSeverity));
                 
                 tmp = rpt_cap2str(entry.ResourceCapabilities);
@@ -124,7 +137,7 @@ SaErrorT discover_domain(SaHpiDomainIdT domain_id, SaHpiSessionIdT session_id, S
                 entitypath2string(&entry.ResourceEntity, tmp_epath, sizeof(tmp_epath));
                 printf("\t%s\n", tmp_epath);
                 printf("\tResourceTag: ");
-                       display_id_string(entry.ResourceTag);
+                       display_textbuffer(entry.ResourceTag);
 
                 if (entry.ResourceCapabilities & SAHPI_CAPABILITY_RDR) 
                         list_rdr(session_id, entry.ResourceId);
@@ -155,8 +168,26 @@ SaErrorT discover_domain(SaHpiDomainIdT domain_id, SaHpiSessionIdT session_id, S
 			break;
         };
 
-	
         return SA_OK;
+}
+
+const char * ctrldigital2str(SaHpiCtrlStateDigitalT digstate) 
+{
+        switch (digstate) {
+	case SAHPI_CTRL_STATE_OFF:
+                return "SAHPI_CTRL_STATE_OFF";
+	case SAHPI_CTRL_STATE_ON:
+                return "SAHPI_CTRL_STATE_ON";
+	case SAHPI_CTRL_STATE_PULSE_OFF:
+                return "SAHPI_CTRL_STATE_PULSE_OFF";
+	case SAHPI_CTRL_STATE_PULSE_ON:
+                return "SAHPI_CTRL_STATE_PULSE_ON";
+	case SAHPI_CTRL_STATE_AUTO:
+                return "SAHPI_CTRL_STATE_AUTO";
+        default:
+		printf("Invalid control digital state=%d\n", digstate);
+                return "INVALID DIGITAL STATE";
+        }
 }
 
 const char * severity2str(SaHpiSeverityT severity)
@@ -175,7 +206,8 @@ const char * severity2str(SaHpiSeverityT severity)
         case SAHPI_DEBUG:
                 return "DEBUG";
         default:
-                return "UNKNOWN SEVERITY";
+		printf("Invalid severity=%d\n", severity);
+                return "INVALID SEVERITY";
         }
 }
 
@@ -216,12 +248,16 @@ const char * get_error_string(SaErrorT error)
                         return "SA_ERR_HPI_NO_RESPONSE";
                 case SA_ERR_HPI_DUPLICATE:
                         return "SA_ERR_HPI_DUPLICATE";
+                case SA_ERR_HPI_UPDATING:
+                        return "SA_ERR_HPI_UPDATING";
                 case SA_ERR_HPI_INITIALIZING:
                         return "SA_ERR_HPI_INITIALIZING";
                 case SA_ERR_HPI_UNKNOWN:
                         return "SA_ERR_HPI_UNKNOWN";
                 case SA_ERR_HPI_INVALID_SESSION:
                         return "SA_ERR_HPI_INVALID_SESSION";
+                case SA_ERR_HPI_INVALID_DOMAIN:
+                        return "SA_ERR_HPI_INVALID_DOMAIN";
                 case SA_ERR_HPI_INVALID_RESOURCE:
                         return "SA_ERR_HPI_INVALID_RESOURCE";
                 case SA_ERR_HPI_INVALID_REQUEST:
@@ -231,7 +267,8 @@ const char * get_error_string(SaErrorT error)
                 case SA_ERR_HPI_UNINITIALIZED:
                         return "SA_ERR_HPI_UNINITIALIZED";
                 default:
-                        return "(invalid error code)";
+			printf("Invalid error code=%d\n", error);
+                        return "(Invalid error code)";
         }
 }
 
@@ -249,7 +286,8 @@ const char * rdrtype2str(SaHpiRdrTypeT type)
         case SAHPI_WATCHDOG_RDR:
                 return "SAHPI_WATCHDOG_RDR";
         default:
-                return "(invalid rdr type)";
+		printf("Invalid RDR type=%d\n", type);
+                return "(Invalid RDR type)";
         }
         return "\0";
 }
@@ -299,94 +337,155 @@ void list_rdr(SaHpiSessionIdT session_id, SaHpiResourceIdT resource_id)
 
 			err = saHpiSensorReadingGet(session_id, resource_id, num, &reading);
 			if (err != SA_OK) {
-				printf("Error reading sensor data {sensor, %d}", num);
+				printf("Error=%d reading sensor data {sensor, %d}\n", err, num);
 				break;
-			} else {
-				if (reading.ValuesPresent & SAHPI_SRF_RAW) {
-					printf("\tValues Present: RAW\n");
-					printf("\t\tRaw value: %d\n", reading.Raw);
-					
-					err = saHpiSensorThresholdsGet(session_id, resource_id, num, &thres);
+			}
 
-					if (thres.LowCritical.ValuesPresent) {
-						printf("\t\tThreshold:  Low Critical Values\n");
-						printreading(thres.LowCritical);
-					}
+			if (reading.ValuesPresent & SAHPI_SRF_RAW) {
+				printf("\tValues Present: RAW\n");
+				printf("\t\tRaw value: %d\n", reading.Raw);
+			}
 
-					if (thres.LowMajor.ValuesPresent) {
-						printf("\t\tThreshold:  Low Major Values\n");
-						printreading(thres.LowMajor);
-					}
+			if (reading.ValuesPresent & SAHPI_SRF_INTERPRETED) {
+				printf("\tValues Present: Interpreted\n");
+				printf("\t\tInterpreted value: %s\n", interpreted2str(reading.Interpreted));
+			}
 
-					if (thres.LowMinor.ValuesPresent) {
-						printf("\t\tThreshold:  Low Minor Values\n");
-						printreading(thres.LowMinor);
-					}
+			if (reading.ValuesPresent & SAHPI_SRF_EVENT_STATE) {
+				printf("\tValues Present: Event State\n");
+			}
 
-					if (thres.UpCritical.ValuesPresent) {
-						printf("\t\tThreshold:  Up Critical Values\n");
-						printreading(thres.UpCritical);
-					}
-
-					if (thres.UpMajor.ValuesPresent) {
-						printf("\t\tThreshold:  Up Major Values\n");
-						printreading(thres.UpMajor);
-					}
-
-					if (thres.UpMinor.ValuesPresent) {
-						printf("\t\tThreshold:  Up Minor Values\n");
-						printreading(thres.UpMinor);
-					}
-
-					if (thres.PosThdHysteresis.ValuesPresent) {
-						printf("\t\tThreshold:  Pos Threshold Hysteresis Values\n");
-						printreading(thres.PosThdHysteresis);
-					}
-
-					if (thres.NegThdHysteresis.ValuesPresent) {
-						printf("\t\tThreshold:  Neg Threshold Hysteresis Values\n");
-						printreading(thres.NegThdHysteresis);
-					}
-
-					if (err != SA_OK &&
-                                            rdr.RdrTypeUnion.SensorRec.ThresholdDefn.IsThreshold == SAHPI_TRUE) {
-						printf("Error reading sensor thresholds {sensor, %d}\n", num);
-						continue;
-					}
+			if (rdr.RdrTypeUnion.SensorRec.ThresholdDefn.IsThreshold == SAHPI_TRUE) {
+				err = saHpiSensorThresholdsGet(session_id, resource_id, num, &thres);
+				if (err != SA_OK) {
+					printf("Error=%d reading sensor thresholds {sensor, %d}\n", err, num);
+					break;
 				}
 				
-				
-				if (reading.ValuesPresent & SAHPI_SRF_INTERPRETED)
-					printf("\tValues Present: Interpreted\n");
-				printf("\t\tInterpreted value: %s\n", interpreted2str(reading.Interpreted));
-
-				if (reading.ValuesPresent & SAHPI_SRF_EVENT_STATE)
-					printf("\tValues Present: Event State\n");
-					
+				if (thres.LowCritical.ValuesPresent) {
+					printf("\t\tThreshold:  Low Critical Values\n");
+					printreading(thres.LowCritical);
+				}
+				if (thres.LowMajor.ValuesPresent) {
+					printf("\t\tThreshold:  Low Major Values\n");
+					printreading(thres.LowMajor);
+				}
+				if (thres.LowMinor.ValuesPresent) {
+					printf("\t\tThreshold:  Low Minor Values\n");
+					printreading(thres.LowMinor);
+				}
+				if (thres.UpCritical.ValuesPresent) {
+					printf("\t\tThreshold:  Up Critical Values\n");
+					printreading(thres.UpCritical);
+				}
+				if (thres.UpMajor.ValuesPresent) {
+					printf("\t\tThreshold:  Up Major Values\n");
+					printreading(thres.UpMajor);
+				}
+				if (thres.UpMinor.ValuesPresent) {
+					printf("\t\tThreshold:  Up Minor Values\n");
+					printreading(thres.UpMinor);
+				}
+				if (thres.PosThdHysteresis.ValuesPresent) {
+					printf("\t\tThreshold:  Pos Threshold Hysteresis Values\n");
+					printreading(thres.PosThdHysteresis);
+				}
+				if (thres.NegThdHysteresis.ValuesPresent) {
+					printf("\t\tThreshold:  Neg Threshold Hysteresis Values\n");
+					printreading(thres.NegThdHysteresis);
+				}
 			}
-			
-				
+		}
+		
+		if (rdr.RdrType == SAHPI_CTRL_RDR)
+		{    
+
+			SaHpiCtrlNumT   num;
+			SaHpiCtrlStateT state;
+			SaHpiCtrlTypeT  type;
+			SaErrorT        err;
+
+			num = rdr.RdrTypeUnion.CtrlRec.Num;
+			err = saHpiControlTypeGet(session_id, resource_id, num, &type);
+			if (err != SA_OK) {
+				printf("Error=%d reading control type {control, %d}\n", err, num);
+				break;
+			}
+			printf("\tControl num: %i\n\tType: %s\n", num, get_control_type(type)); 
+		
+			err = saHpiControlStateGet(session_id, resource_id, num, &state);
+			if (err != SA_OK) {
+				printf("Error=%d reading control state {control, %d}\n", err, num);
+				break;
+			}
+			if (type != state.Type) {
+				printf("Control Type mismatch between saHpiControlTypeGet=%d and saHpiControlStateGet = %d\n", 
+				       type, state.Type);
+			}
+
+			switch (state.Type) {
+			case SAHPI_CTRL_TYPE_DIGITAL:
+				printf("Control Digital State=%s\n", ctrldigital2str(state.StateUnion.Digital));
+			case SAHPI_CTRL_TYPE_DISCRETE:
+				printf("Control Discrete State=%x\n", state.StateUnion.Discrete);
+			case SAHPI_CTRL_TYPE_ANALOG:
+				printf("Control Analog State=%x\n", state.StateUnion.Analog);
+			case SAHPI_CTRL_TYPE_STREAM:
+				printf("Control Stream Repeat=%d\n", state.StateUnion.Stream.Repeat);
+				printf("Control Stream Data=");
+				display_oembuffer(state.StateUnion.Stream.StreamLength, state.StateUnion.Stream.Stream);
+			case SAHPI_CTRL_TYPE_TEXT:
+				printf("Control Text Line Num=%c\n", state.StateUnion.Text.Line);
+				display_textbuffer(state.StateUnion.Text.Text);
+			case SAHPI_CTRL_TYPE_OEM:
+				printf("Control OEM Manufacturer=%d\n", state.StateUnion.Oem.MId);
+				printf("Control OEM Data=");
+				display_oembuffer((SaHpiUint32T)state.StateUnion.Oem.BodyLength, 
+						  state.StateUnion.Oem.Body);
+			default:
+				printf("Invalid control type=%d from saHpiControlStateGet\n", state.Type);
+			}
 		}
 
                 printf("\tEntity: \n");
                 entitypath2string(&rdr.Entity, tmp_epath, sizeof(tmp_epath));
                 printf("\t\t%s\n", tmp_epath);
                 printf("\tIdString: ");
-                display_id_string(rdr.IdString);
+                display_textbuffer(rdr.IdString);
                 printf("\n"); /* Produce blank line between rdrs. */
         }while(next_rdr != SAHPI_LAST_ENTRY);
 }
 
-void display_id_string(SaHpiTextBufferT string)
+void display_oembuffer(SaHpiUint32T length, SaHpiUint8T *string)
+{
+        int i;
+
+	for (i = 0; i < length; i++) {
+		printf("%c", string[i]);
+	}
+
+        printf("\n");
+}
+
+
+void display_textbuffer(SaHpiTextBufferT string)
 {
         int i;
         switch(string.DataType) {
+        case SAHPI_TL_TYPE_BINARY:
+                for (i = 0; i < string.DataLength; i++)
+                        printf("%x", string.Data[i]);
+                break;
+        case SAHPI_TL_TYPE_BCDPLUS:
+                for (i = 0; i < string.DataLength; i++)
+                        printf("%c", string.Data[i]);
+                break;
         case SAHPI_TL_TYPE_ASCII6:
                 for (i = 0; i < string.DataLength; i++)
                         printf("%c", string.Data[i]);
                 break;
         default:
-                printf("Unsupported string type");
+                printf("Invalid string data type=%d", string.DataType);
         }
         printf("\n");
 }
@@ -429,6 +528,7 @@ char * interpreted2str (SaHpiSensorInterpretedT interpreted)
 			sprintf(str, "%s", interpreted.Value.SensorBuffer);
 			break;
 		default:
+			printf("Invalid interpreted type=%d\n", interpreted.Type);
 			break;
 	}
         
@@ -471,6 +571,30 @@ char * rpt_cap2str (SaHpiCapabilitiesT ResourceCapabilities)
 	return answer;
 }
 
+const char * get_control_type(SaHpiCtrlTypeT type) 
+{
+	switch(type) {
+		case SAHPI_CTRL_TYPE_DIGITAL:
+			return "DIGITAL CONTROL";
+		case SAHPI_CTRL_TYPE_DISCRETE:
+			return "DISCRETE CONTROL";
+		case SAHPI_CTRL_TYPE_ANALOG:
+			return "ANALOG CONTROL";
+		case SAHPI_CTRL_TYPE_STREAM:
+			return "STREAM CONTROL";
+		case SAHPI_CTRL_TYPE_TEXT:
+			return "TEXT CONTROL";
+		case SAHPI_CTRL_TYPE_OEM:
+			return "OEM CONTROL";
+		default:
+			printf("Invalid control type=%d\n", type);
+			return "Invalid Control Type";
+	}
+
+	return "\0";
+
+}
+
 const char * get_sensor_type(SaHpiSensorTypeT type) 
 {
 	switch(type) {
@@ -478,8 +602,14 @@ const char * get_sensor_type(SaHpiSensorTypeT type)
 			return "TEMPERATURE SENSOR";
 		case SAHPI_VOLTAGE:
 			return "VOLTAGE SENSOR";
+		case SAHPI_CURRENT:
+			return "CURRENT SENSOR";
 		case SAHPI_FAN:
 			return "FAN SENSOR";
+		case SAHPI_PHYSICAL_SECURITY:
+			return "PHYSICAL SECURITY";
+		case SAHPI_PLATFORM_VIOLATION:
+			return "PLATFORM_VIOLATION";
 		case SAHPI_PROCESSOR:
 			return "PROCESSOR";
 		case SAHPI_POWER_SUPPLY:
@@ -488,76 +618,75 @@ const char * get_sensor_type(SaHpiSensorTypeT type)
 			return "POWER UNIT";
 		case SAHPI_COOLING_DEVICE:
 			return "COOLING DEVICE";
-		case SAHPI_MEMORY:
-			return "MEMORY";
 		case SAHPI_OTHER_UNITS_BASED_SENSOR:
 			return "OTHER UNITS BASED SENSOR";
-		case SAHPI_LAN:
-			return "Lan Sensor";
-    		case SAHPI_PHYSICAL_SECURITY:
-			return("SAHPI_PHYSICAL_SECURITY"); 
-    		case SAHPI_PLATFORM_VIOLATION:
-			return("SAHPI_PLATFORM_VIOLATION"); 
+		case SAHPI_MEMORY:
+			return "MEMORY";
     		case SAHPI_DRIVE_SLOT:
-			return("SAHPI_DRIVE_SLOT"); 
+			return "SAHPI_DRIVE_SLOT"; 
     		case SAHPI_POST_MEMORY_RESIZE:
-			return("SAHPI_POST_MEMORY_RESIZE"); 
+			return "SAHPI_POST_MEMORY_RESIZE"; 
     		case SAHPI_SYSTEM_FW_PROGRESS:
-			return("SAHPI_SYSTEM_FW_PROGRESS"); 
+			return "SAHPI_SYSTEM_FW_PROGRESS"; 
     		case SAHPI_EVENT_LOGGING_DISABLED:
-			return("SAHPI_EVENT_LOGGING_DISABLED"); 
+			return "SAHPI_EVENT_LOGGING_DISABLED"; 
     		case SAHPI_RESERVED1:
-			return("SAHPI_RESERVED1"); 
+			return "SAHPI_RESERVED1"; 
     		case SAHPI_SYSTEM_EVENT:
-			return("SAHPI_SYSTEM_EVENT"); 
+			return "SAHPI_SYSTEM_EVENT"; 
     		case SAHPI_CRITICAL_INTERRUPT:
-			return("SAHPI_CRITICAL_INTERRUPT"); 
+			return "SAHPI_CRITICAL_INTERRUPT"; 
     		case SAHPI_BUTTON:
-			return("SAHPI_BUTTON"); 
+			return "SAHPI_BUTTON"; 
     		case SAHPI_MODULE_BOARD:
-			return("SAHPI_MODULE_BOARD"); 
+			return "SAHPI_MODULE_BOARD"; 
     		case SAHPI_MICROCONTROLLER_COPROCESSOR:
-			return("SAHPI_MICROCONTROLLER_COPROCESSOR"); 
+			return "SAHPI_MICROCONTROLLER_COPROCESSOR"; 
     		case SAHPI_ADDIN_CARD:
-			return("SAHPI_ADDIN_CARD"); 
+			return "SAHPI_ADDIN_CARD"; 
     		case SAHPI_CHASSIS:
-			return("SAHPI_CHASSIS"); 
+			return "SAHPI_CHASSIS"; 
     		case SAHPI_CHIP_SET:
-			return("SAHPI_CHIP_SET"); 
+			return "SAHPI_CHIP_SET"; 
     		case SAHPI_OTHER_FRU:
-			return("SAHPI_OTHER_FRU"); 
+			return "SAHPI_OTHER_FRU"; 
     		case SAHPI_CABLE_INTERCONNECT:
-			return("SAHPI_CABLE_INTERCONNECT"); 
+			return "SAHPI_CABLE_INTERCONNECT"; 
     		case SAHPI_TERMINATOR:
-			return("SAHPI_TERMINATOR"); 
+			return "SAHPI_TERMINATOR"; 
     		case SAHPI_SYSTEM_BOOT_INITIATED:
-			return("SAHPI_SYSTEM_BOOT_INITIATED"); 
+			return "SAHPI_SYSTEM_BOOT_INITIATED"; 
     		case SAHPI_BOOT_ERROR:
-			return("SAHPI_BOOT_ERROR"); 
+			return "SAHPI_BOOT_ERROR"; 
     		case SAHPI_OS_BOOT:
-			return("SAHPI_OS_BOOT"); 
+			return "SAHPI_OS_BOOT"; 
     		case SAHPI_OS_CRITICAL_STOP:
-			return("SAHPI_OS_CRITICAL_STOP"); 
+			return "SAHPI_OS_CRITICAL_STOP"; 
     		case SAHPI_SLOT_CONNECTOR:
-			return("SAHPI_SLOT_CONNECTOR"); 
+			return "SAHPI_SLOT_CONNECTOR"; 
     		case SAHPI_SYSTEM_ACPI_POWER_STATE:
-			return("SAHPI_SYSTEM_ACPI_POWER_STATE"); 
+			return "SAHPI_SYSTEM_ACPI_POWER_STATE"; 
     		case SAHPI_RESERVED2:
-			return("SAHPI_RESERVED2"); 
+			return "SAHPI_RESERVED2"; 
     		case SAHPI_PLATFORM_ALERT:
-			return("SAHPI_PLATFORM_ALERT"); 
+			return "SAHPI_PLATFORM_ALERT"; 
     		case SAHPI_ENTITY_PRESENCE:
-			return("SAHPI_ENTITY_PRESENCE"); 
+			return "SAHPI_ENTITY_PRESENCE"; 
     		case SAHPI_MONITOR_ASIC_IC:
-			return("SAHPI_MONITOR_ASIC_IC"); 
+			return "SAHPI_MONITOR_ASIC_IC"; 
+		case SAHPI_LAN:
+			return "SAHPI_LAN";
     		case SAHPI_MANAGEMENT_SUBSYSTEM_HEALTH:
-			return("SAHPI_MANAGEMENT_SUBSYSTEM_HEALTH"); 
+			return "SAHPI_MANAGEMENT_SUBSYSTEM_HEALTH"; 
     		case SAHPI_BATTERY:
-			return("SAHPI_BATTERY"); 
-    		case SAHPI_OEM_SENSOR && SAHPI_OEM_SENSOR == 0xC:
-			return("SAHPI_OEM_SENSOR=0xC"); 
+			return "SAHPI_BATTERY"; 
+    		case SAHPI_OPERATIONAL:
+			return "SAHPI_OPERATIONAL=0xA"; 
+    		case SAHPI_OEM_SENSOR:
+			return "SAHPI_OEM_SENSOR=0xC"; 
 		default:
-			return "Other";
+			printf("Invalid sensor type=%d\n", type);
+			return "Invalid Sensor Type";
 	}
 
 return "\0";
@@ -596,11 +725,11 @@ const char * get_sensor_category (SaHpiEventCategoryT category)
 			return "SAHPI_EC_USER";
 		case SAHPI_EC_GENERIC:
 			return "SAHPI_EC_GENERIC";
-			
                 default:
-			return "Unknown Category";
-			
+			printf("Invalid sensor category=%d\n", category);
+			return "Invalid Category";
 	}
+
 	return("\0");
 }
 
