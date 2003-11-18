07a3d279df071c8484d5f0694b1b7ae82adba467
jnfleisc <jnfleisc@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2003-11-18 21:58:17 +0000

    Removed copyright from header (added in error)

--- examples/test_watchdog.c
+++ examples/test_watchdog.c
@@ -1,7 +1,5 @@
 /*      -*- linux-c -*-
  *
- * Copyright (c) 2003 by Intel Corp.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
