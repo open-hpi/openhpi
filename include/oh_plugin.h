4bd00254d9ec25ffe7814f22278432cb0e13dfb3
yzhuang <yzhuang@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2003-12-01 06:20:45 +0000

    reverse last unsuccessful patch

--- include/oh_plugin.h
+++ include/oh_plugin.h
@@ -423,6 +423,9 @@ struct oh_abi_v1 {
 
 };
 
+/*The function is used for plugin loader to get interface*/
+int get_interface(void **pp, const uuid_t uuid);
+
 /* Structure for static plugins */
 typedef int (*get_interface_t)( void **pp, const uuid_t uuid );
 
