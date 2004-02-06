f7c87c90ef5be14cbbffc153ebae11e224c7f5a1
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-02-06 13:30:30 +0000

    Add selcache to handler state

--- include/oh_plugin.h
+++ include/oh_plugin.h
@@ -24,6 +24,7 @@
 #include <sys/time.h>
 #include <SaHpi.h>
 #include <rpt_utils.h>
+#include <sel_utils.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -133,6 +134,7 @@ struct oh_event {
 
 struct oh_handler_state {
         RPTable *rptcache;
+	oh_sel  *selcache;
         GSList *eventq;
         GHashTable *config;
         void *data;
