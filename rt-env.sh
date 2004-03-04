6322361185ea3d227d80b7bca9c54b52687d4a75
yzhuang <yzhuang@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-04 05:47:22 +0000

    Remove bundle of files which are imported mistakenly

--- rt-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -x
-#
-OPENHPI_CONF=./examples/openhpi.conf
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:./src/.libs 
-PLUGIN_ROOT=./plugins
-OPENHPI_PATH=${PLUGIN_ROOT}/dummy:${PLUGIN_ROOT}/ipmi:${PLUGIN_ROOT}/ipmidirect:${PLUGIN_ROOT}/watchdog:${PLUGIN_ROOT}/sysfs:${PLUGIN_ROOT}/text_remote:${PLUGIN_ROOT}/snmp_bc:${LTDL_LIBRARY_PATH}
-
-export OPENHPI_CONF LD_LIBRARY_PATH OPENHPI_PATH
