29cf35f785d36160ede98fb587f99d52d980aa11
sesherman <sesherman@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-09 21:46:19 +0000

    Add event translation; Add RID to OEM Events

--- plugins/snmp_bc/snmp_bc_event.h
+++ plugins/snmp_bc/snmp_bc_event.h
@@ -16,17 +16,19 @@
 #ifndef __SNMP_BC_EVENT_H
 #define __SNMP_BC_EVENT_H
 
+#define MAX_THRESHOLD_VALUE_STRINGSIZE 10
+
 /* Global BC Event Number to HPI Event Hash Table */
 GHashTable * event2hpi_hash;
-
-
 
 int event2hpi_hash_init(void);
 int event2hpi_hash_free(void);
 
 int find_res_events(SaHpiEntityPathT *ep, const struct BC_ResourceInfo *bc_res_info);
 int find_sensor_events(SaHpiEntityPathT *ep, SaHpiSensorNumT sid, const struct snmp_bc_sensor *rpt_sensor);
+
 int log2event(void *hnd, gchar *logstr, SaHpiEventT *event, int isdst);
+int bcsrc2rid(void *hnd, gchar *src, SaHpiResourceIdT *rid);
 
 int snmp_bc_add_to_eventq(void *hnd, SaHpiEventT *thisEvent);
 #endif
