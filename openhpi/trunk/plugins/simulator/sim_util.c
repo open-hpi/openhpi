397d33a3d7651b4b2601a15b646416ae218dadc0
guorj <guorj@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-10 01:01:51 +0000

    add copyright

--- openhpi/trunk/plugins/simulator/sim_util.c
+++ openhpi/trunk/plugins/simulator/sim_util.c
@@ -1,3 +1,18 @@
+/*      -*- linux-c -*-
+ *
+ * Copyright (c) 2003 by Intel Corp.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authours:
+ *      Racing Guo <racing.guo@intel.com>
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
