9ebd694d192502a190d836ca1bb25fa1732ce0b8
wdashley <wdashley@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-22 19:27:46 +0000

    Added sensor support routines.

---
+++ plugins/snmp_rsa/snmp_rsa_sensor.h
@@ -0,0 +1,46 @@
+/*      -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2004
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Author(s):
+ *      Steve Sherman <sesherman@us.ibm.com>
+ *      W. David Ashley <dashley@us.ibm.com>
+ */
+
+#ifndef SNMP_RSA_SENSOR_H
+#define SNMP_RSA_SENSOR_H
+
+SaErrorT snmp_rsa_get_sensor_data(void *hnd,
+				  SaHpiResourceIdT id,
+				  SaHpiSensorNumT num,
+				  SaHpiSensorReadingT *data);
+
+SaErrorT snmp_rsa_get_sensor_thresholds(void *hnd,
+				        SaHpiResourceIdT id,
+				        SaHpiSensorNumT num,
+				        SaHpiSensorThresholdsT *thres);
+
+SaErrorT snmp_rsa_set_sensor_thresholds(void *hnd,
+				        SaHpiResourceIdT id,
+				        SaHpiSensorNumT num,
+				        const SaHpiSensorThresholdsT *thres);
+
+
+SaErrorT snmp_rsa_get_sensor_event_enables(void *hnd,
+					   SaHpiResourceIdT id,
+					   SaHpiSensorNumT num,
+					   SaHpiSensorEvtEnablesT *enables);
+
+SaErrorT snmp_rsa_set_sensor_event_enables(void *hnd,
+	        			   SaHpiResourceIdT id,
+	        			   SaHpiSensorNumT num,
+					   const SaHpiSensorEvtEnablesT *enables);
+
+#endif /* SNMP_RSA_SENSOR_H */
