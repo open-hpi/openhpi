28f878c247df0a85551c5a73576e37ca124d4056
sesherman <sesherman@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-23 21:40:05 +0000

    First wave of event definitions

--- plugins/snmp_bc/t/snmp_bc_session.c
+++ plugins/snmp_bc/t/snmp_bc_session.c
@@ -96,7 +96,7 @@ void *snmp_bc_open(GHashTable *handler_config)
 		}
 
 		if (!strcmp(get_value.string, SNMP_BC_BCE_MODEL)) {
-			strcpy(bc_type, SNMP_BC_PLATFORM_BCE);
+			strcpy(bc_type, SNMP_BC_PLATFORM_BC);
 		}
 		else if (!strcmp(get_value.string, SNMP_BC_BCT_MODEL_AC)) {
 			strcpy(bc_type, SNMP_BC_PLATFORM_BCT);
@@ -113,7 +113,7 @@ void *snmp_bc_open(GHashTable *handler_config)
 	printf("************************************\n");
 	printf("****** BladeCenter Simulator  ******\n");
 
-	if (!strcmp(bc_type, SNMP_BC_PLATFORM_BCE)) {
+	if (!strcmp(bc_type, SNMP_BC_PLATFORM_BC)) {
 		printf("****** BladeCenter Integrated ******\n");
 	}
 	if (!strcmp(bc_type, SNMP_BC_PLATFORM_BCT)) {
