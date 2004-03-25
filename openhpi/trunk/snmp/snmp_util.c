a66a8cb34117eefd9d6aca861df20446a1222b2d
djudkovi <djudkovi@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-25 19:23:24 +0000

    ArtifactID:923348 Comment:moved code from snmp_client_utils.c to snmp/snmp_util.c[h]

--- openhpi/trunk/snmp/snmp_util.c
+++ openhpi/trunk/snmp/snmp_util.c
@@ -11,11 +11,14 @@
  *
  * Authors:
  *      Renier Morales <renierm@users.sf.net>
+ *	David Judkovics <djudkovi@us.ibm.com>
  *
  */
 
 #include <snmp_util.h>
 #include <string.h>
+
+#include <openhpi.h>
 
 /**
  * snmp_get
@@ -227,3 +230,243 @@ int snmp_get_bulk(struct snmp_session *ss, const char *objid,
         return value->type? 0 : -1;
 }
 #endif
+
+/**
+ * snmp_get2: Gets a single value indicated by the objectid
+ * using snmp.
+ * @handle: a handle to the snmp session needed to make an
+ * snmp transaction.
+ * @objid: string containing the OID entry.
+ * @value: the value received from snmp will be put in this union.
+ *
+ * In the case of multiple values being returned, the type in 'value' will be
+ * ASN_NULL (0x05). Nothing else in 'value' will be filled in.
+ * Use snmp_get_all for doing gets that return multiple values.
+ *
+ * Return value: Returns 0 if successful, -1 if there was an error.
+ **/
+SaErrorT snmp_get2(struct snmp_session *ss, 
+	           oid *objid,
+	           size_t objid_len,
+                   struct snmp_value *value) 
+{
+        struct snmp_pdu *pdu;
+        struct snmp_pdu *response;
+        
+        struct variable_list *vars;
+        int status;
+        
+        /*
+         * Create the PDU for the data for our request.
+         */
+        pdu = snmp_pdu_create(SNMP_MSG_GET);
+
+        snmp_add_null_var(pdu, objid, objid_len);
+
+        /*
+         * Send the Request out.
+         */
+        status = snmp_synch_response(ss, pdu, &response);
+
+        /*
+         * Process the response.
+         */
+        if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
+
+                vars = response->variables;
+
+                value->type = vars->type;
+
+                if (vars->next_variable != NULL) {
+
+                        value->type = ASN_NULL;
+
+		/* If there are more values, set return type to null. */
+                } else if ( (vars->type == ASN_INTEGER) || 
+			    (vars->type == ASN_COUNTER) || 
+			    (vars->type == ASN_UNSIGNED) ) {
+
+			value->integer = *(vars->val.integer);
+
+                } else {
+
+                        value->str_len = vars->val_len;
+
+                        if (value->str_len >= MAX_ASN_STR_LEN)
+                                value->str_len = MAX_ASN_STR_LEN;
+
+                        else value->string[value->str_len] = '\0';
+                        
+                        memcpy(value->string, vars->val.string, value->str_len);
+                       
+                }
+
+		/* display data */
+#ifdef DEBUG
+		if (CHECK_END(vars->type)) { 
+			fprintf(stderr, "*** snmp_get2 ******************************************\n");
+			fprint_variable(stderr, 
+					vars->name, 
+					vars->name_length, 
+					vars);
+			fprintf(stderr, "********************************************************\n");
+		}  else {
+			dbg("snmp_get2(): No idea.Unknown Type: %X", vars->type);
+			fprint_variable(stderr, 
+					vars->name, 
+					vars->name_length, 
+					vars);
+		}		
+#endif
+        } else {
+		int i;
+                value->type = (u_char)0x00; /* Set return type to 0 in case of error. */
+                if (status == STAT_SUCCESS) {
+                        dbg("Error, Reason: %s", 
+				snmp_errstring(response->errstat));
+			fprintf(stderr, "objid: ");
+			for(i = 0; i<objid_len; i++ )
+				fprintf(stderr, "%d.", (int)objid[i]);
+			fprintf(stderr, "\n");
+                } else
+                        snmp_sess_perror("snmpget", ss);
+        }
+
+        /* Clean up: free the response */
+	sc_free_pdu(&response); 
+
+        return value->type? SA_OK : SA_ERR_HPI_ERROR;
+}
+
+
+/**
+ * snmp_set2: Gets a single value indicated by the objectid
+ * using snmp.
+ * @handle: a handle to the snmp session needed to make an
+ * snmp transaction.
+ * @objid: string containing the OID entry.
+ * @value: the value received from snmp will be put in this union.
+ *
+ * In the case of multiple values being returned, the type in 'value' will be
+ * ASN_NULL (0x05). Nothing else in 'value' will be filled in.
+ * Use snmp_get_all for doing gets that return multiple values.
+ *
+ * Return value: Returns 0 if successful, -1 if there was an error.
+ **/
+SaErrorT snmp_set2(struct snmp_session *ss, 
+	           oid *objid,
+	           size_t objid_len,
+                   struct snmp_value *value) 
+{
+	struct snmp_pdu *pdu;
+	struct snmp_pdu *response;
+
+        char datatype = 's';
+        void *dataptr = NULL;
+        int status = 0;
+	int rtncode = 0;
+
+        /*
+         * Create the PDU for the data for our request.
+         */
+        pdu = snmp_pdu_create(SNMP_MSG_SET);
+
+        rtncode = 0; /* Default - All is OK */
+
+        switch (value->type)
+        {
+                case ASN_INTEGER:
+                case ASN_COUNTER:
+                        datatype = 'i';
+			(long *)dataptr = &value->integer;
+                        break;
+                case ASN_OCTET_STR:
+                        datatype = 's';
+			(u_char *)dataptr = &value->string;
+                        break;
+                default:
+                        rtncode = -1;
+                        dbg("datatype %c not yet supported by snmp_set2()", 
+				value->type);
+                        break;
+        }
+
+        if (rtncode == 0) {
+
+		/*
+		 * Set the data to send out
+		 */
+                snmp_add_var(pdu, objid, objid_len, datatype, dataptr);
+        	/*
+         	* Send the Request out.
+         	*/
+        	status = snmp_synch_response(ss, pdu, &response);
+        	/*
+         	* Process the response.
+         	*/
+        	if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
+			/* display data */
+#ifdef DEBUG
+			fprintf(stderr, "*** snmp_set2 ******************************************\n");
+				if (CHECK_END(response->variables->type)) {
+					fprint_variable(stderr, 
+							response->variables->name,
+							response->variables->name_length,
+							response->variables);  
+				}  else 
+					fprintf(stderr, "snmp_set2(): No idea.\n");
+			fprintf(stderr, "********************************************************\n");
+#endif
+                	rtncode = 0;
+                
+        	} else {
+                	rtncode = -1;
+                	if (status == STAT_SUCCESS) 
+                        	dbg("snmp_set2: Error in packet, Reason: %s",
+					snmp_errstring(response->errstat));
+                	else
+                        	snmp_sess_perror("snmpset", ss);
+        	}
+
+        	/* Clean up: free the response */
+        	if (response) snmp_free_pdu(response);
+	
+	}
+
+        return rtncode;
+
+}
+
+int snmp_getn_bulk( struct snmp_session *ss, 
+		    oid *bulk_objid, 
+		    size_t bulk_objid_len,
+		    struct snmp_pdu *bulk_pdu, 
+		    struct snmp_pdu **bulk_response,
+		    int num_repetitions )
+{
+		int status;
+
+	bulk_pdu = snmp_pdu_create(SNMP_MSG_GETBULK);
+ 	
+	bulk_pdu->non_repeaters = 0; 
+	
+	bulk_pdu->max_repetitions = num_repetitions;
+
+	snmp_add_null_var(bulk_pdu, bulk_objid, bulk_objid_len);
+	
+	/* Send the Request out.*/
+	status = snmp_synch_response(ss, bulk_pdu, bulk_response);
+
+	return(status);
+
+}
+
+void sc_free_pdu(struct snmp_pdu **p) 
+{
+	if (*p) {
+		snmp_free_pdu(*p);
+		*p = NULL;
+	}
+}
+
+
