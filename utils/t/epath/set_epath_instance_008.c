b6372539b81594d745740e7c3ffa64a5e9e6949e
cchia <cchia@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-30 18:01:20 +0000

    ArtifactID: 880143 Comment: unit tests for entity path utility set_epath_instance

---
+++ utils/t/epath/set_epath_instance_008.c
@@ -0,0 +1,86 @@
+/* -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2004
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *     Chris Chia <cchia@users.sf.net>
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <SaHpi.h>
+#include <epath_utils.h>
+
+/**
+ * set_epath_instance test8.
+ *   call set_epath_instance with 4 element entity path, victim element at head 
+ *   expected result: only head element's instance number changed
+ *
+ * Return value: 0 on success, 1 on failure
+ **/
+int main(int argc, char **argv)
+{
+        SaHpiEntityPathT ep = {{{SAHPI_ENT_ADD_IN_CARD, 101},
+                                {SAHPI_ENT_POWER_MODULE, 2020},
+                                {SAHPI_ENT_POWER_MGMNT, 30303},
+                                {SAHPI_ENT_SUB_CHASSIS, 404040},
+                                {0}}};
+        SaHpiEntityInstanceT x = 555555;
+        int mydebug = 0;
+         
+        if (mydebug) printf(" test8\n");
+        if(set_epath_instance(&ep, SAHPI_ENT_ADD_IN_CARD, x)) {
+                if (mydebug) printf("set_ep_inst test8 checkpoint 1 failed\n");
+                return 1;
+        }
+        if (ep.Entry[0].EntityInstance != x) {
+                if (mydebug) printf("set_ep_inst test8 failed, entInst %d != %d\n",
+                                   ep.Entry[0].EntityInstance, x);
+                return 1;
+        }
+        if (ep.Entry[0].EntityType != SAHPI_ENT_ADD_IN_CARD) {
+                if (mydebug) printf("set_ep_inst test8 failed, entType %d != SAHPI_ENT_ADD_IN_CARD\n",
+                                   ep.Entry[0].EntityType);
+                return 1;
+        }
+        if (ep.Entry[1].EntityInstance != 2020) {
+                if (mydebug) printf("set_ep_inst test8 failed, entInst %d != 2020\n",
+                                   ep.Entry[1].EntityInstance);
+                return 1;
+        }
+        if (ep.Entry[1].EntityType != SAHPI_ENT_POWER_MODULE) {
+                if (mydebug) printf("set_ep_inst test8 failed, entType %d != SAHPI_ENT_POWER_MODULE\n",
+                                   ep.Entry[1].EntityType);
+                return 1;
+        }
+        if (ep.Entry[2].EntityInstance != 30303) {
+                if (mydebug) printf("set_ep_inst test8 failed, entInst %d != 30303\n",
+                                   ep.Entry[2].EntityInstance);
+                return 1;
+        }
+        if (ep.Entry[2].EntityType != SAHPI_ENT_POWER_MGMNT) {
+                if (mydebug) printf("set_ep_inst test8 failed, entType %d! = SAHPI_ENT_POWER_MGMNT\n",
+                                   ep.Entry[2].EntityType);
+                return 1;
+        }
+        if (ep.Entry[3].EntityInstance != 404040) {
+                if (mydebug) printf("set_ep_inst test8 failed, entInst %d != 404040\n",
+                                   ep.Entry[3].EntityInstance);
+                return 1;
+        }
+        if (ep.Entry[3].EntityType != SAHPI_ENT_SUB_CHASSIS) {
+                if (mydebug) printf("set_ep_inst test8 failed, entType %d != SAHPI_ENT_SUB_CHASSIS\n",
+                                   ep.Entry[3].EntityType);
+                return 1;
+        }
+
+        return 0;
+}
