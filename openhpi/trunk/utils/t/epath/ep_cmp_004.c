e25fec47a78c7b12363ef90473ed2641d1661652
cchia <cchia@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-30 18:03:21 +0000

    ArtifactID: 880143 Comment: unit test for entity path utility ep_cmp

---
+++ openhpi/trunk/utils/t/epath/ep_cmp_004.c
@@ -0,0 +1,57 @@
+/* -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2004
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *     Chris Chia <cchia@users.sf.net>
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <SaHpi.h>
+#include <epath_utils.h>
+
+/**
+ * ep_cmp test4
+ *   multi element entity path comarison, expect success
+ *
+ * Return value: 0 on success, 1 on failure
+ **/
+int main(int argc, char **argv)
+{
+        SaHpiEntityPathT ep1 = {{{SAHPI_ENT_ADD_IN_CARD,151},
+                                 {SAHPI_ENT_FRONT_PANEL_BOARD,252},
+                                 {SAHPI_ENT_BACK_PANEL_BOARD,353},
+                                 {SAHPI_ENT_POWER_SYSTEM_BOARD,454},
+                                 {SAHPI_ENT_DRIVE_BACKPLANE,555},
+                                 {SAHPI_ENT_SYS_EXPANSION_BOARD,656},
+                                 {SAHPI_ENT_OTHER_SYSTEM_BOARD,757},
+                                 {SAHPI_ENT_PROCESSOR_BOARD,858},
+                                 {0}}};
+        SaHpiEntityPathT ep2 = {{{SAHPI_ENT_ADD_IN_CARD,151},
+                                 {SAHPI_ENT_FRONT_PANEL_BOARD,252},
+                                 {SAHPI_ENT_BACK_PANEL_BOARD,353},
+                                 {SAHPI_ENT_POWER_SYSTEM_BOARD,454},
+                                 {SAHPI_ENT_DRIVE_BACKPLANE,555},
+                                 {SAHPI_ENT_SYS_EXPANSION_BOARD,656},
+                                 {SAHPI_ENT_OTHER_SYSTEM_BOARD,757},
+                                 {SAHPI_ENT_PROCESSOR_BOARD,858},
+                                 {0}}};
+        int mydebug = 0;
+
+        if (ep_cmp(&ep1, &ep2)) {
+                if (mydebug) printf("ep_cmp test4 failed\n");
+                return 1;
+        }
+
+        if (mydebug) printf("ep_cmp test4 OK\n");
+        return 0;
+}
