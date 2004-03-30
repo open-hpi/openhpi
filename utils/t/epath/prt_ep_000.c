b505a88c0a5022a959b8046400acbf185657f498
cchia <cchia@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-30 18:05:45 +0000

    ArtifactID: 880143 Comment: unit test for entity path utility prt_ep

---
+++ utils/t/epath/prt_ep_000.c
@@ -0,0 +1,39 @@
+                                            /* -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2004
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *     Chris Chia <cchia@users.sf.net>
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <SaHpi.h>
+#include <epath_utils.h>
+
+/**
+ * prt_ep test0
+ *          call prt_ep with NULL pointer entity path and expect the unknown
+ * Return value: 0 on success, 1 on failure
+ **/
+int main(int argc, char **argv)
+{
+        int mydebug = 0;
+
+        if (prt_ep(NULL) == 0) {
+                if (mydebug) printf ("Null pointer entity path printed\n"); 
+        }    
+        else {
+                if (mydebug) printf ("Null pointer entity path can\'t be printed\n");
+        }    
+
+        return 0;
+}
