0e0e742bb18af9e86fea0d7475c8c7aa01d7d965
cchia <cchia@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-30 18:03:21 +0000

    ArtifactID: 880143 Comment: unit test for entity path utility ep_cmp

---
+++ utils/t/epath/ep_cmp_005.c
@@ -0,0 +1,48 @@
+/* -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2004
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *     Chris Chia <cchia@users.sf.net>
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <SaHpi.h>
+#include <epath_utils.h>
+
+/**
+ * ep_cmp test1
+ *   full element entity path comarison, expect success
+ *
+ * Return value: 0 on success, 1 on failure
+ **/
+int main(int argc, char **argv)
+{
+        SaHpiEntityPathT ep1;
+        SaHpiEntityPathT ep2;
+        int i;
+        int mydebug = 0;
+
+        for (i=0; i<SAHPI_MAX_ENTITY_PATH; i++) {
+                ep1.Entry[i].EntityType = SAHPI_ENT_IO_SUBBOARD;
+                ep1.Entry[i].EntityInstance = i+10;
+                ep2.Entry[i].EntityType = SAHPI_ENT_IO_SUBBOARD;
+                ep2.Entry[i].EntityInstance = i+10;
+        }
+        if (ep_cmp(&ep1, &ep2)) {
+                if (mydebug) printf("ep_cmp test1 failed\n");
+                return 1;
+        }
+
+        if (mydebug) printf("ep_cmp test1 OK\n");
+        return 0;
+}
---
+++ utils/t/epath/ep_cmp_006.c
@@ -0,0 +1,40 @@
+/* -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2004
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *     Chris Chia <cchia@users.sf.net>
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <SaHpi.h>
+#include <epath_utils.h>
+
+/**
+ * ep_cmp test6
+ *   null pointer test, expect error return code from calling ep_cmp
+ *
+ * Return value: 0 on success, 1 on failure
+ **/
+int main(int argc, char **argv)
+{
+        SaHpiEntityPathT ep;
+        int mydebug = 0;
+
+        if (ep_cmp(NULL, &ep)==0) {
+                if (mydebug) printf("ep_cmp test6 failed\n");
+                return 1;
+        }
+
+        if (mydebug) printf("ep_cmp test6 OK\n");
+        return 0;
+}
