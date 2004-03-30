5a5a09bc0ce1cff35894bac8837139cda82e3ca6
cchia <cchia@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-30 18:01:20 +0000

    ArtifactID: 880143 Comment: unit tests for entity path utility set_epath_instance

---
+++ openhpi/trunk/utils/t/epath/set_epath_instance_006.c
@@ -0,0 +1,86 @@
+/* -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2004
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *     Chris Chia <cchia@users.sf.net>
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <SaHpi.h>
+#include <epath_utils.h>
+
+/**
+ * set_epath_instance test6.
+ *   call set_epath_instance with entity path that has 4 elements, victim element in the middle
+ *   expected result: only victim element's instance number changed
+ *
+ * Return value: 0 on success, 1 on failure
+ **/
+int main(int argc, char **argv)
+{
+        SaHpiEntityPathT ep = {{{SAHPI_ENT_FAN, 11099},
+                                {SAHPI_ENT_BATTERY, 2002},
+                                {SAHPI_ENT_RACK, 37373},
+                                {SAHPI_ENT_DISK_BAY, 440044},
+                                {0}}};
+        SaHpiEntityInstanceT x = 123456;
+        int mydebug = 0;
+         
+        if (mydebug) printf(" test6\n");
+        if(set_epath_instance(&ep, SAHPI_ENT_RACK, x)) {
+                if (mydebug) printf("set_ep_inst test6 checkpoint 1 failed\n");
+                return 1;
+        }
+        if (ep.Entry[2].EntityInstance != x) {
+                if (mydebug) printf("set_ep_inst test6 failed, entInst %d != %d\n",
+                                   ep.Entry[2].EntityInstance, x);
+                return 1;
+        }
+        if (ep.Entry[2].EntityType != SAHPI_ENT_RACK) {
+                if (mydebug) printf("set_ep_inst test6 failed, entType %d != SAHPI_ENT_RACK\n",
+                                   ep.Entry[2].EntityType);
+                return 1;
+        }
+        if (ep.Entry[0].EntityInstance != 11099) {
+                if (mydebug) printf("set_ep_inst test6 failed, entInst %d != 11099\n",
+                                   ep.Entry[0].EntityInstance);
+                return 1;
+        }
+        if (ep.Entry[0].EntityType != SAHPI_ENT_FAN) {
+                if (mydebug) printf("set_ep_inst test6 failed, entType %d != SAHPI_ENT_FAN\n",
+                                   ep.Entry[0].EntityType);
+                return 1;
+        }
+        if (ep.Entry[1].EntityInstance != 2002) {
+                if (mydebug) printf("set_ep_inst test6 failed, entInst %d != 2002\n",
+                                   ep.Entry[1].EntityInstance);
+                return 1;
+        }
+        if (ep.Entry[1].EntityType != SAHPI_ENT_BATTERY) {
+                if (mydebug) printf("set_ep_inst test6 failed, entType %d! = SAHPI_ENT_BATTERY\n",
+                                   ep.Entry[1].EntityType);
+                return 1;
+        }
+        if (ep.Entry[3].EntityInstance != 440044) {
+                if (mydebug) printf("set_ep_inst test6 failed, entInst %d != 440044\n",
+                                   ep.Entry[3].EntityInstance);
+                return 1;
+        }
+        if (ep.Entry[3].EntityType != SAHPI_ENT_DISK_BAY) {
+                if (mydebug) printf("set_ep_inst test6 failed, entType %d != SAHPI_ENT_DISK_BAY\n",
+                                   ep.Entry[3].EntityType);
+                return 1;
+        }
+
+        return 0;
+}
---
+++ openhpi/trunk/utils/t/epath/set_epath_instance_007.c
@@ -0,0 +1,86 @@
+/* -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2004
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *     Chris Chia <cchia@users.sf.net>
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <SaHpi.h>
+#include <epath_utils.h>
+
+/**
+ * set_epath_instance test7.
+ *   call set_epath_instance with entity path that has 4 elements, victim element in middle
+ *   expected result: only victim element's instance number changed
+ *
+ * Return value: 0 on success, 1 on failure
+ **/
+int main(int argc, char **argv)
+{
+        SaHpiEntityPathT ep = {{{SAHPI_ENT_INTERCONNECT, 1515},
+                                {SAHPI_ENT_SYSTEM_SLOT, 2525},
+                                {SAHPI_ENT_SUBRACK, 3535},
+                                {SAHPI_ENT_IO_SUBBOARD, 4545},
+                                {0}}};
+        SaHpiEntityInstanceT x = 98765;
+        int mydebug = 0;
+         
+        if (mydebug) printf(" test7\n");
+        if(set_epath_instance(&ep, SAHPI_ENT_SYSTEM_SLOT, x)) {
+                if (mydebug) printf("set_ep_inst test7 checkpoint 1 failed\n");
+                return 1;
+        }
+        if (ep.Entry[1].EntityInstance != x) {
+                if (mydebug) printf("set_ep_inst test7 failed, entInst %d != %d\n",
+                                   ep.Entry[1].EntityInstance, x);
+                return 1;
+        }
+        if (ep.Entry[1].EntityType != SAHPI_ENT_SYSTEM_SLOT) {
+                if (mydebug) printf("set_ep_inst test7 failed, entType %d != SAHPI_ENT_SYSTEM_SLOT\n",
+                                   ep.Entry[1].EntityType);
+                return 1;
+        }
+        if (ep.Entry[0].EntityInstance != 1515) {
+                if (mydebug) printf("set_ep_inst test7 failed, entInst %d != 1515\n",
+                                   ep.Entry[0].EntityInstance);
+                return 1;
+        }
+        if (ep.Entry[0].EntityType != SAHPI_ENT_INTERCONNECT) {
+                if (mydebug) printf("set_ep_inst test7 failed, entType %d != SAHPI_ENT_INTERCONNECT\n",
+                                   ep.Entry[0].EntityType);
+                return 1;
+        }
+        if (ep.Entry[2].EntityInstance != 3535) {
+                if (mydebug) printf("set_ep_inst test7 failed, entInst %d != 3535\n",
+                                   ep.Entry[2].EntityInstance);
+                return 1;
+        }
+        if (ep.Entry[2].EntityType != SAHPI_ENT_SUBRACK) {
+                if (mydebug) printf("set_ep_inst test7 failed, entType %d != SAHPI_ENT_SUBRACK\n",
+                                   ep.Entry[2].EntityType);
+                return 1;
+        }
+        if (ep.Entry[3].EntityInstance != 4545) {
+                if (mydebug) printf("set_ep_inst test7 failed, entInst %d != 4545\n",
+                                   ep.Entry[3].EntityInstance);
+                return 1;
+        }
+        if (ep.Entry[3].EntityType != SAHPI_ENT_IO_SUBBOARD) {
+                if (mydebug) printf("set_ep_inst test7 failed, entType %d! = SAHPI_ENT_IO_SUBBOARD\n",
+                                   ep.Entry[3].EntityType);
+                return 1;
+        }
+
+        return 0;
+}
