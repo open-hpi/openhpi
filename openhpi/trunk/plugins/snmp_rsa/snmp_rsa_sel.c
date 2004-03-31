73f8741873c2739920c4a3db87d3fbb77fc2f36d
wdashley <wdashley@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-31 20:20:47 +0000

    ArtifactID: 926768 Comment: Fixed function calls to the event APIs.

--- openhpi/trunk/plugins/snmp_rsa/snmp_rsa_sel.c
+++ openhpi/trunk/plugins/snmp_rsa/snmp_rsa_sel.c
@@ -23,7 +23,7 @@
 #include <rsa_resources.h>
 #include <snmp_rsa.h>
 #include <snmp_rsa_sel.h>
-// #include <snmp_rsa_event.h>
+#include <snmp_rsa_event.h>
 
 
 /**
@@ -320,17 +320,17 @@ int snmp_rsa_sel_read_add (void *hnd, SaHpiResourceIdT id, SaHpiSelEntryIdT curr
         SaHpiSelEntryT tmpentry;
         char oid[50];
         SaErrorT rv;
-//	int isdst = 0;
+	int isdst = 0;
 
 	sprintf(oid, "%s.%d", RSA_SEL_ENTRY_OID, current);
 	snmp_get(custom_handle->ss,oid,&get_value);
 
 	if(get_value.type == ASN_OCTET_STR) {
-//              log2event(hnd, get_value.string, &tmpentry.Event, isdst);
+              log2event(hnd, get_value.string, &tmpentry.Event, isdst);
                 tmpentry.EntryId = current;
                 tmpentry.Timestamp = tmpentry.Event.Timestamp;
                 rv = oh_sel_add(handle->selcache, &tmpentry);
-//              rv = snmp_rsa_add_to_eventq(hnd, &tmpentry.Event);
+              rv = snmp_rsa_add_to_eventq(hnd, &tmpentry.Event);
 	} else {
 		dbg("Couldn't fetch SEL Entry from RSA snmp");
 		return -1;
