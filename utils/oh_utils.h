1a275fa0607f0fa0321b3879bf44051490da3147
renierm <renierm@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-03-31 08:48:23 +0000

    ArtifactId:922540 Comment: Consolidating openhpiutils headers

---
+++ utils/oh_utils.h
@@ -0,0 +1,36 @@
+/*      -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2004
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *      Renier Morales <renierm@users.sf.net>
+ *
+ */
+
+#ifndef OH_UTILS_H
+#define OH_UTILS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <epath_utils.h>
+#include <uid_utils.h>
+#include <rpt_utils.h>
+#include <sel_utils.h>
+#include <ecode_utils.h>
+#include <printevent_utils.h>
+#include <sensor_utils.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
