b325b8d93cb8a157391fe03f14a9be9c312b0e43
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-04-07 12:42:13 +0000

    Add ShowSel util

--- utils/printevent_utils.h
+++ utils/printevent_utils.h
@@ -41,6 +41,8 @@ extern "C" {
  * Export function prototypes
 */
 void print_event(SaHpiEventT *thisEvent);
+void ShowSel( SaHpiSelEntryT  *sel, SaHpiRdrT *rdr,
+                     		SaHpiRptEntryT *rptentry );
 char *decode_enum(struct code2string *code_array, int NUM_MAX, int code);
 void saftime2str(SaHpiTimeT time, char * str, size_t size);
 
