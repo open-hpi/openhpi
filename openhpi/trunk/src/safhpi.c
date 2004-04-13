53ed2006e3231c359b5acabdca724b510a773b06
sdague <sdague@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-04-13 18:53:50 +0000

    ArtifactID: 933751 Comment: added data_access_unlock to top level macros so they can be safely used in setup for apis that need a global lock

--- openhpi/trunk/src/safhpi.c
+++ openhpi/trunk/src/safhpi.c
@@ -40,12 +40,13 @@ static enum {
         OH_STAT_READY,
 } oh_hpi_state = OH_STAT_UNINIT;
 
-#define OH_STATE_READY_CHECK                                    \
-        do {                                                    \
-                if (OH_STAT_READY!=oh_hpi_state) {              \
-                        dbg("Uninitialized HPI");               \
-                        return SA_ERR_HPI_UNINITIALIZED;        \
-                }                                               \
+#define OH_STATE_READY_CHECK                                      \
+        do {                                                      \
+                if (OH_STAT_READY != oh_hpi_state) {              \
+                        dbg("Uninitialized HPI");                 \
+                        data_access_unlock();                     \
+                        return SA_ERR_HPI_UNINITIALIZED;          \
+                }                                                 \
         } while(0)                                               
 
 /*
@@ -54,11 +55,12 @@ static enum {
  * function.
  */
 
-#define OH_SESSION_SETUP(sid, ses)      \
-        do {                                              \
+#define OH_SESSION_SETUP(sid, ses)                         \
+        do {                                               \
                 ses = session_get(sid);                    \
                 if (!s) {                                  \
                         dbg("Invalid SessionId %d", sid);  \
+                        data_access_unlock();              \
                         return SA_ERR_HPI_INVALID_SESSION; \
                 }                                          \
         } while (0)
@@ -67,15 +69,15 @@ static enum {
  * OH_HANDLER_GET gets the hander for the rpt and resource id.  It
  * returns INVALID PARAMS if the handler isn't there
  */
-#define OH_HANDLER_GET(rpt,rid,h)                                             \
-        do {                                                                  \
+#define OH_HANDLER_GET(rpt,rid,h)                                       \
+        do {                                                            \
                 struct oh_resource_data *rd = oh_get_resource_data(rpt, rid); \
-                if(!rd || !rd->handler) {                                     \
-                        dbg("Can't find handler for Resource %d", rid);       \
-                        return SA_ERR_HPI_INVALID_PARAMS;                     \
-                }                                                             \
-                                                                              \
-                h = rd->handler;                                              \
+                if(!rd || !rd->handler) {                               \
+                        dbg("Can't find handler for Resource %d", rid); \
+                        data_access_unlock();                           \
+                        return SA_ERR_HPI_INVALID_PARAMS;               \
+                }                                                       \
+                h = rd->handler;                                        \
         } while(0)
 
 /* 
@@ -83,13 +85,14 @@ static enum {
  * is actually valid, returning badly if it isn't
  */
 
-#define OH_RPT_GET(SessionId, rpt) \
-        do { \
-                rpt = default_rpt;              \
-                if(rpt == NULL) { \
+#define OH_RPT_GET(SessionId, rpt)                                     \
+        do {                                                           \
+                rpt = default_rpt;                                     \
+                if(rpt == NULL) {                                      \
                         dbg("RPT for Session %d not found",SessionId); \
-                        return SA_ERR_HPI_INVALID_SESSION; \
-                } \
+                        data_access_unlock();                          \
+                        return SA_ERR_HPI_INVALID_SESSION;             \
+                }                                                      \
         } while(0)
 
 
@@ -102,6 +105,7 @@ static enum {
                 r = oh_get_resource_by_id(rpt, rid);            \
                 if(!r) {                                        \
                         dbg("Resource %d doesn't exist", rid);  \
+                        data_access_unlock();                   \
                         return SA_ERR_HPI_INVALID_RESOURCE;     \
                 }                                               \
         } while(0)
@@ -276,7 +280,7 @@ SaErrorT SAHPI_API saHpiSessionOpen(
 {
         struct oh_session *s;
         int rv;
-
+        
         if (SecurityParams != NULL) {
                 dbg("SecurityParams must be NULL");
                 return SA_ERR_HPI_INVALID_PARAMS;
@@ -406,12 +410,7 @@ SaErrorT SAHPI_API saHpiRptEntryGet(
 
         data_access_lock();
 
-        s = session_get(SessionId);
-        if (!s) {
-                dbg("Invalid session");
-                data_access_unlock();
-                return SA_ERR_HPI_INVALID_SESSION;
-        }
+        OH_SESSION_SETUP(SessionId, s);
 
         OH_RPT_GET(SessionId, rpt);
         
@@ -453,13 +452,13 @@ SaErrorT SAHPI_API saHpiRptEntryGetByResourceId(
         RPTable *rpt;
         SaHpiRptEntryT *req_entry;
 
+        data_access_lock();
+
         OH_STATE_READY_CHECK;
         
         OH_SESSION_SETUP(SessionId, s);
 
         OH_RPT_GET(SessionId, rpt);
-
-        data_access_lock();
 
         req_entry = oh_get_resource_by_id(rpt, ResourceId);
 
