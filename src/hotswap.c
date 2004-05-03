745d8be680f2f380628cb76f48cae8eda5933ab9
thkanne <thkanne@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-05-03 15:35:55 +0000

    ArtifactID: 947026 Comment: cleanup of debug code.

--- src/hotswap.c
+++ src/hotswap.c
@@ -82,14 +82,6 @@ void process_hotswap_policy(struct oh_handler *handler)
 
                 gettimeofday1(&cur);
 
-/*
-                char str[1024] = "invalid";
-                SaHpiRptEntryT *rpte = oh_get_resource_by_id( default_rpt, e.parent );
-                entitypath2string( &rpte->ResourceEntity, str, 1024 );
-                printf( "hotswap event: %s\n", str );
-                print_event( &e.event );
-*/
-
                 if (e.event.EventDataUnion.HotSwapEvent.HotSwapState 
                                 == SAHPI_HS_STATE_INSERTION_PENDING) {
                         est = e.event.Timestamp + get_hotswap_auto_insert_timeout();
