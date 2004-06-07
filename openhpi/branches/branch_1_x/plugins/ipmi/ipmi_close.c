d9f75b8458c9d0cbdd2ede80093b3b456f6a1cb7
tariqx <tariqx@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-06-07 17:17:23 +0000

    ArtifactID: Comment: Merged from 0.9 branch all approved fixes

--- openhpi/branches/branch_1_x/plugins/ipmi/ipmi_close.c
+++ openhpi/branches/branch_1_x/plugins/ipmi/ipmi_close.c
@@ -15,6 +15,8 @@
 
 #include "ipmi.h"
 
+static int close_flag;
+
 static void close_done(void *cb_data)
 {
 		struct oh_handler_state *handler = (struct oh_handler_state *)cb_data;
@@ -24,17 +26,25 @@ static void close_done(void *cb_data)
 		
 		g_free(ipmi_handler);
 		g_free(handler);
+		close_flag = 1;
 }
 		
 
 void ohoi_close_connection(ipmi_domain_t *domain, void *user_data)
 {
 		struct oh_handler_state *handler = (struct oh_handler_state *)user_data;
+		struct ohoi_handler *ipmi_handler = (struct ohoi_handler *)handler->data;
 
 		int rv;
 
 		rv = ipmi_close_connection(domain, close_done, handler);
 		if (rv)
 				dbg("ipmi_close_connection failed!");
+
+		close_flag = 0;
+
+		while (close_flag != 1) {
+				sel_select(ipmi_handler->ohoi_sel, NULL, 0, NULL, NULL);
+		}
 
 }
