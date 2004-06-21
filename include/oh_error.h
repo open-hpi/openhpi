4f394d3ab52134395cc4b0af3bf0052fba9556a5
sdague <sdague@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-06-21 21:19:08 +0000

    ArtifactID: 976994 Comment: made OHDEBUG be OPENHPI_DEBUG instead

--- include/oh_error.h
+++ include/oh_error.h
@@ -11,6 +11,7 @@
  *
  * Author(s):
  *      Renier Morales <renierm@users.sf.net>
+ *
  */
 
 #ifndef OH_ERROR_H
@@ -26,8 +27,8 @@ extern "C" {
 
 #define dbg(format, ...)                                                \
         do {                                                            \
-                if (getenv("OHDEBUG") != NULL) {                        \
-                        if (strcmp((char *)getenv("OHDEBUG"),"YES") == 0) { \
+                if (getenv("OPENHPI_DEBUG") != NULL) {                  \
+                        if (strcmp((char *)getenv("OPENHPI_DEBUG"),"YES") == 0) { \
                                 fprintf(stderr, "%s:%d:%s: ", __FILE__, __LINE__, __func__); \
                                 fprintf(stderr, format "\n", ## __VA_ARGS__); \
                         }                                               \
