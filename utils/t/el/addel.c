42c7711cacc610733d1c2ba1a9fb30fbdcf482ff
wdashley <wdashley@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-08-04 14:17:43 +0000

    ArtifactID: 1002038 Comment: Modified the el utilities to accept the rdr and rpt structs to enable a context for the log entry. Also modified the get function to return the full context. Deleted the add and delete functions as they are not valid for the HPIB spec.

--- utils/t/el/addel.c
+++ utils/t/el/addel.c
@@ -51,7 +51,7 @@ static char *data[10] = {
 * Note that the event data we use is just a string from the above array
 */
 int add_event(oh_el *el, int idx) {
-        SaHpiEventLogEntryT *entry, *fetchentry;
+        oh_el_entry *entry, *fetchentry;
         SaHpiEventT event;
         SaErrorT retc;
         SaHpiEventLogEntryIdT oldId = el->nextId, next, prev;
@@ -67,21 +67,21 @@ int add_event(oh_el *el, int idx) {
         event.Timestamp = SAHPI_TIME_UNSPECIFIED;
         event.Severity = SAHPI_DEBUG;
         strcpy((char *) &event.EventDataUnion.UserEvent.UserEventData.Data, data[idx]);
-        retc = oh_el_append(el, &event);
+        retc = oh_el_append(el, &event, NULL, NULL);
 
         if (retc != SA_OK) {
                 dbg("ERROR: oh_el_add failed.");
                 return 1;
         }
 	
-	entry = (SaHpiEventLogEntryT *)(g_list_last(el->elentries)->data);
+	entry = (oh_el_entry *)(g_list_last(el->elentries)->data);
         /* check correct id */
-        if (entry->EntryId != el->nextId - 1) {
+        if (entry->event.EntryId != el->nextId - 1) {
                 dbg("ERROR: entry.EntryId invalid.");
                 return 1;
         }
 
-        if (entry->Timestamp == 0) {
+        if (entry->event.Timestamp == 0) {
                 dbg("ERROR: entry.Timestamp invalid.");
                 return 1;
         }
@@ -97,7 +97,7 @@ int add_event(oh_el *el, int idx) {
                 return 1;
         }
 
-        if(el->lastUpdate != entry->Timestamp) {
+        if(el->lastUpdate != entry->event.Timestamp) {
                 dbg("ERROR: el->lastUpdate invalid.");
                 return 1;
         }
@@ -118,57 +118,57 @@ int add_event(oh_el *el, int idx) {
         }
 
         /* now fetch the event and compare it */
-        retc = oh_el_get(el, entry->EntryId, &prev, &next, &fetchentry);
+        retc = oh_el_get(el, entry->event.EntryId, &prev, &next, &fetchentry);
         if (retc != SA_OK) {
                 dbg("ERROR: oh_el_get failed.");
                 return 1;
         }
 
-        if (entry->EntryId != fetchentry->EntryId) {
+        if (entry->event.EntryId != fetchentry->event.EntryId) {
                 dbg("ERROR: entry->EntryId invalid.");
                 return 1;
         }
 
-        if (prev != entry->EntryId - 1) {
+        if (prev != entry->event.EntryId - 1) {
                 if (prev != SAHPI_NO_MORE_ENTRIES) {
                         dbg("ERROR: prev invalid.");
                         return 1;
                 }
         }
 
-        if (next != entry->EntryId + 1) {
+        if (next != entry->event.EntryId + 1) {
                 if (next != SAHPI_NO_MORE_ENTRIES) {
                         dbg("ERROR: next invalid.");
                         return 1;
                 }
         }
 
-        if (fetchentry->Timestamp == 0) {
+        if (fetchentry->event.Timestamp == 0) {
                 dbg("ERROR: fetchentry->Timestamp invalid.");
                 return 1;
         }
 
-        if (fetchentry->Event.Source != 1) {
+        if (fetchentry->event.Event.Source != 1) {
                 dbg("ERROR: fetchentry->Event.Source invalid.");
                 return 1;
         }
 
-        if (fetchentry->Event.EventType != SAHPI_ET_USER) {
+        if (fetchentry->event.Event.EventType != SAHPI_ET_USER) {
                 dbg("ERROR: fetchentry->Event.EventType invalid.");
                 return 1;
         }
 
-        if (fetchentry->Event.Timestamp != SAHPI_TIME_UNSPECIFIED) {
+        if (fetchentry->event.Event.Timestamp != SAHPI_TIME_UNSPECIFIED) {
                 dbg("ERROR: fetchentry->Event.Timestamp invalid.");
                 return 1;
         }
 
-        if (fetchentry->Event.Severity != SAHPI_DEBUG) {
+        if (fetchentry->event.Event.Severity != SAHPI_DEBUG) {
                 dbg("ERROR: fetchentry->Event.Severity invalid.");
                 return 1;
         }
 
-        if (strcmp((char *)fetchentry->Event.EventDataUnion.UserEvent.UserEventData.Data,
+        if (strcmp((char *)fetchentry->event.Event.EventDataUnion.UserEvent.UserEventData.Data,
                     data[idx])) {
                 dbg("ERROR: fetchentry->Event.EventDataUnion.UserEvent.UserEventData invalid.");
                 return 1;
