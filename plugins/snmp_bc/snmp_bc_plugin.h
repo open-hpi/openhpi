a2824e9e27988985731b940e3f565c9607317ebd
sesherman <sesherman@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-08-06 13:19:45 +0000

    ArtifactId: 983632 Comment: Get utilities to compile

---
+++ plugins/snmp_bc/snmp_bc_plugin.h
@@ -0,0 +1,36 @@
+/*      -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2004
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Author(s):
+ *      Steve Sherman <stevees@us.ibm.com>
+ */
+
+#ifndef SNMP_BC_PLUGIN_H
+#define SNMP_BC_PLUGIN_H
+
+#include <bc_errorlog.h>
+#include <bc_resources.h>
+#include <bc_str2event.h>
+#include <snmp_bc_control.h>
+#include <snmp_bc_discover.h>
+#include <snmp_bc_event.h>
+#include <snmp_bc.h>
+#include <snmp_bc_hotswap.h>
+#include <snmp_bc_inventory.h>
+#include <snmp_bc_plugin.h>
+#include <snmp_bc_sel.h>
+#include <snmp_bc_sensor.h>
+#include <snmp_bc_session.h>
+#include <snmp_bc_time.h>
+#include <snmp_bc_utils.h>
+#include <snmp_bc_watchdog.h>
+
+#endif
