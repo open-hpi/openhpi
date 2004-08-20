75688387006a6f79370850682b30ac728e453d51
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-08-20 03:54:46 +0000

    Convert to command line, no interactive mode

--- clients/hpitree.c
+++ clients/hpitree.c
@@ -27,14 +27,11 @@
 #include <getopt.h>
 #include <SaHpi.h> 
 #include <oh_utils.h>
-#include <pthread.h>
 
 
 /* 
  * Function prototypes
  */
-void *sahpi_discover_thread(void *sessionid);
-void usage(void);
 SaErrorT list_all(SaHpiSessionIdT sessionid);
 SaErrorT list_rpt(SaHpiSessionIdT sessionid);
 SaErrorT list_inv(SaHpiSessionIdT sessionid, SaHpiResourceIdT res_id);
@@ -42,29 +39,25 @@ SaErrorT list_sens(SaHpiSessionIdT sessionid, SaHpiResourceIdT res_id);
 SaErrorT list_ctrl(SaHpiSessionIdT sessionid, SaHpiResourceIdT res_id);
 SaErrorT list_wdog(SaHpiSessionIdT sessionid, SaHpiResourceIdT res_id);
 SaErrorT list_ann (SaHpiSessionIdT sessionid, SaHpiResourceIdT res_id);
+SaHpiResourceIdT getid(char *optarg);
 
 SaErrorT walkInventory(SaHpiSessionIdT sessionid,
 		   SaHpiResourceIdT resourceid,
 		   SaHpiIdrInfoT *idrInfo);
 
+#define all_resources 255
 
 /* 
  * Globals for this driver
  */
-char progver[] = "0.1 HPI-B";
+char progver[] = "0.2 HPI-B";
 char progname[] = "hpitree";
-int thread = 1;
 int fdebug = 0;
-int fthread = 0;
-
-#define get_resourceid \
-do { \
-	list_rpt(sessionid); \
-	printf("Enter resource id:"); \
-	p = fgets(input, 255, stdin); \
-	if ((p = strchr(input, '\n')) != NULL)	*p = '\0'; \
-} while(0)
-
+int f_listall = 0;
+int f_rpt     = 0;
+int f_sensor  = 0;
+int f_inv     = 0;
+int f_ctrl    = 0;
 
 /* 
  * Main                
@@ -76,25 +69,40 @@ main(int argc, char **argv)
 	
 	SaHpiVersionT	hpiVer;
 	SaHpiSessionIdT sessionid;
+	SaHpiResourceIdT resourceid = all_resources;
 
-	char input[255], *p;
-	pthread_t discover_thread;
-	void *thread_done;
-	int valid = 0;
 	int c;
 	    
 	printf("\n\n%s ver %s\n",argv[0],progver);
-	while ( (c = getopt( argc, argv,"xt?")) != EOF ) {
+	while ( (c = getopt( argc, argv,"arsicx?")) != EOF ) {
 		switch(c) {
-			case 't': fthread = 1; break;
+			case 'a': f_listall = 1; break;
+			case 'r': f_rpt     = 1; break;
+			case 's': f_sensor  = 1; 
+				  /* resourceid = getid(optarg); */
+				  break;
+			case 'i': f_inv     = 1;
+				  /* resourceid = getid(optarg); */
+				  break;
+			case 'c': f_ctrl    = 1;
+				  /* resourceid = getid(optarg); */
+				  break;
 			case 'x': fdebug = 1; break;
 			default:
-				printf("Usage: %s [-x] [-t]\n", progname);
-				printf("   -x  Display debug messages\n");
-				printf("   -t  Enable resource (re)discovery thread\n\n\n\n");
+				printf("\n\tUsage: %s [-option]\n\n", progname);
+				printf("\t      (No Option) Display all rpt's and rdr's\n");
+				printf("\t           -a     Display all rpt's and rdr's\n");
+				printf("\t           -c     Display only controls\n");
+				printf("\t           -i     Display only inventories\n");
+				printf("\t           -r     Display only rpt's\n");
+				printf("\t           -s     Display only sensors\n");
+				printf("\t           -x     Display debug messages\n");
+				printf("\n\n\n\n");
 				exit(1);
 		}
 	}
+ 
+	if (argc == 1) f_listall = 1;
 
 	/* 
 	 * House keeping:
@@ -115,130 +123,44 @@ main(int argc, char **argv)
 	       	printf("saHpiSessionOpen returns with SessionId %d\n", sessionid);
 
 	/*
-	 * Initial discovery
+	 * Resource discovery
 	 */
 	if (fdebug) printf("saHpiDiscover\n");
 	rv = saHpiDiscover(sessionid);
-	if ((rv != SA_OK) || fdebug) {
+	if (rv != SA_OK) {
 		printf("saHpiDiscover returns %s\n",oh_lookup_error(rv));
+		exit(-1);
 	}
 
-	printf("Initial discovery done\n");
-
-	if(fthread) {
-		printf("Start (re)discovery thread\n");
-		rv = pthread_create(&discover_thread, NULL, sahpi_discover_thread,
-							      (void *)sessionid);
-		if (rv)
-			printf("Error creating event thread\n");
-	}
-
-	while (!valid) {
+	printf("Discovery done\n");
         
-		printf("\n\n\t%s ver %s\n",argv[0],progver);
-		printf("\tEnter a command or \"help\" for list of commands\n");
-		printf("Command> ");
-
-		fflush(stdout);
-		p = fgets(input, 255, stdin);
-        
-		if ((p = strchr(input, '\n')) != NULL)
-						*p = '\0';
-        	if (!strcmp(input, "all")) {
-			list_all(sessionid);
-			
-        	} else if (!strcmp(input, "rpt")) {
-			list_rpt(sessionid);
-			
-        	} else if (!strcmp(input, "inv")) { 
-			get_resourceid;
-			list_inv(sessionid, (SaHpiResourceIdT) atoi(input));
-			        
-		} else if (!strcmp(input, "all_inv")) {
-			list_inv(sessionid, 0XFF);
-			
-        	} else if (!strcmp(input, "sensor")) {
-			get_resourceid;
-			list_sens(sessionid, (SaHpiResourceIdT) atoi(input));
-			
-        	} else if (!strcmp(input, "all_sensors")) {
-			list_sens(sessionid, 0XFF);
-			
-        	} else if (!strcmp(input, "cntl")) {
-			/*get_resourceid;*/
-			list_ctrl(sessionid, (SaHpiResourceIdT) atoi(input));
-			
-        	} else if (!strcmp(input, "all_cntl")) {
-			list_ctrl(sessionid, 0XFF);
-									
-        	} else if (!strcmp(input, "ann")) {
-			/*get_resourceid; */
-			list_ann(sessionid, (SaHpiResourceIdT) atoi(input));
-									
-        	} else if (!strcmp(input, "all_ann")) {
-			list_ann(sessionid, 0XFF);
-									
-        	} else if (!strcmp(input, "wdog")) {
-			/*get_resourceid; */
-			list_wdog(sessionid, (SaHpiResourceIdT) atoi(input));
-									
-        	} else if (!strcmp(input, "all_wdog")) {
-			list_wdog(sessionid, 0XFF);
-			
-		} else if (!strcmp(input, "help")) {
-			usage();
-			
-		} else if (!strcmp(input, "?")) {
-			usage();
-			
-		} else if (!strcmp(input, "quit")) {
-			valid = 1;
-			thread = 0;
-
-			if (fthread) {
-				rv = pthread_join(discover_thread, &thread_done);
-				if (rv)
-					printf("Error joining thread\n");
-				printf("Discovery %s\n", (char *)thread_done);
-			}
-			break;
-
-		} else {
-			printf("Type command (help for list) or \"quit\" to exit\n");
-			printf("Command> ");
-		}
-   	}
-
-
+	if(f_listall) 
+		list_all(sessionid);
+	else {	
+		if(f_rpt) list_rpt(sessionid);
+		if(f_sensor) list_sens(sessionid,resourceid);
+		if(f_inv) list_inv(sessionid, resourceid); 
+		if(f_ctrl) list_ctrl(sessionid, resourceid);
+	}	
 
 	rv = saHpiSessionClose(sessionid);
-	/* rv = saHpiFinalize();	*/
 	
 	exit(0);
 }
 
-/* 
- * Get user inputs
- * User requests are reflected in global flags
-**/
-void usage(void)
-{
-	printf("Available commands:\n"
-		"\tall \t\t-- Dump all\n"
-	       	"\trpt \t\t-- Dump all resources in domain RPTable\n"		
-	       	"\tinv \t\t-- Dump inventory data for a given resource (by ID)\n"
-               	"\tall_inv \t-- Dump all inventory data in a session\n"
-	       	"\tsensor \t\t-- Dump sensors data for a given resource (by ID)\n"
-               	"\tall_sensors \t-- Dump all sensor data in a session\n"
-	       	"\tcntl \t\t-- Dump controls data for a given resource (by ID)\n"
-               	"\tall_cntl \t-- Dump all controls data in a session\n"
-	       	"\tann \t\t-- Dump annuntiator data for a given resource(by ID)\n"
-               	"\tall_ann \t-- Dump all annuntiator data in a session\n"
-	       	"\twdog \t\t-- Dump watchdog data for a given resource -- (by ID)\n"
-               	"\tall_wdog \t-- Dump all watchdog data in a session\n");
-}
 
-/* 
+/*
+ *
+ */
+SaHpiResourceIdT getid(char *optarg) 
+{
+	if (strncmp(optarg, "all", 3)) 
+		return(atoi(optarg));
+	else 
+		return(all_resources);
+
+}
+/*
  *
  */
 SaErrorT list_rpt(SaHpiSessionIdT sessionid)
@@ -463,27 +385,6 @@ SaErrorT walkInventory(	SaHpiSessionIdT sessionid,
 	return(rv);
 }
 
-/* 
- *
- */
-void *sahpi_discover_thread(void *sessionid)
-{
-	int rv;
-	struct timeval to;
-
-	while (thread == 1) {
-		rv = saHpiDiscover((SaHpiSessionIdT) sessionid);
-		if (rv != SA_OK)
-			printf("discovery failed\n");
-
-		to.tv_sec = 3;  /*check everything 3 seconds */
-		to.tv_usec = 0;
-
-		select(0, 0, NULL, NULL, &to);
-	}
-
-	pthread_exit("Thread exit\n");
-}
 
 /* 
  *
