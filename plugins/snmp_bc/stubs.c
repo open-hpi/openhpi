883c6f185d466c5bf97f6fb9923078c40e47c5d7
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-09-14 15:01:36 +0000

    Correct sensor_event_enable parm list

--- plugins/snmp_bc/stubs.c
+++ plugins/snmp_bc/stubs.c
@@ -42,7 +42,7 @@ SaErrorT snmp_bc_get_sensor_event_enables(void *hnd,
 SaErrorT snmp_bc_set_sensor_event_enables(void *hnd,
 					  SaHpiResourceIdT id,
 					  SaHpiSensorNumT num,
-					  const SaHpiBoolT *enables)
+					  const SaHpiBoolT enables)
 {
   return(SA_ERR_HPI_UNSUPPORTED_API);
 }
