174b903b38fa8f062df1a04e99bfad5524d1d4fc
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-09-17 02:42:05 +0000

    Enabling watchdog build

--- plugins/snmp_bc/stubs.c
+++ plugins/snmp_bc/stubs.c
@@ -2,34 +2,6 @@
 
 #include <snmp_bc_plugin.h>
 
-#if 0
-SaErrorT snmp_bc_get_sensor_reading(void *hnd,
-				 SaHpiResourceIdT id,
-				 SaHpiSensorNumT num,
-				 SaHpiSensorReadingT *data, 
-				 SaHpiEventStateT    *state)
-{
-  return(SA_ERR_HPI_UNSUPPORTED_API);
-}
-
-
-SaErrorT snmp_bc_get_sensor_thresholds(void *hnd,
-				       SaHpiResourceIdT id,
-				       SaHpiSensorNumT num,
-				       SaHpiSensorThresholdsT *thres)
-{
-  return(SA_ERR_HPI_UNSUPPORTED_API);
-}
-
-SaErrorT snmp_bc_set_sensor_thresholds(void *hnd,
-				       SaHpiResourceIdT id,
-				       SaHpiSensorNumT num,
-				       const SaHpiSensorThresholdsT *thres)
-{
-  return(SA_ERR_HPI_UNSUPPORTED_API);
-}
-
-#endif
 
 SaErrorT snmp_bc_get_sensor_event_enables(void *hnd,
 					  SaHpiResourceIdT id,
@@ -47,52 +19,6 @@ SaErrorT snmp_bc_set_sensor_event_enables(void *hnd,
   return(SA_ERR_HPI_UNSUPPORTED_API);
 }
 
-
-#if 0
-SaErrorT snmp_bc_get_control_state(void *hnd, 
-				   SaHpiResourceIdT id,
-				   SaHpiCtrlNumT num,
-				   SaHpiCtrlModeT *mode,
-				   SaHpiCtrlStateT *state)
-{
-  return(SA_ERR_HPI_UNSUPPORTED_API);
-}
-
-
-SaErrorT snmp_bc_set_control_state(void *hnd,
-				   SaHpiResourceIdT id,
-				   SaHpiCtrlNumT num,
-				   SaHpiCtrlModeT *mode,
-				   SaHpiCtrlStateT *state)
-{
-  return(SA_ERR_HPI_UNSUPPORTED_API);
-}
-#endif
-
-#if 0
-SaErrorT snmp_bc_get_watchdog_info(void *hnd,
-				   SaHpiResourceIdT id,
-				   SaHpiWatchdogNumT num,
-				   SaHpiWatchdogT *wdt)
-{
-  return(SA_ERR_HPI_UNSUPPORTED_API);
-}
-
-SaErrorT snmp_bc_set_watchdog_info(void *hnd,
-				   SaHpiResourceIdT id,
-				   SaHpiWatchdogNumT num,
-				   SaHpiWatchdogT *wdt)
-{
-  return(SA_ERR_HPI_UNSUPPORTED_API);
-}
-#endif
-
-SaErrorT snmp_bc_reset_watchdog(void *hnd,
-				SaHpiResourceIdT id,
-				SaHpiWatchdogNumT num)
-{
-  return(SA_ERR_HPI_UNSUPPORTED_API);
-}
 
 SaErrorT snmp_bc_get_hotswap_state(void *hnd,
 				   SaHpiResourceIdT id,
