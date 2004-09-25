3aceebaebe3f4a6b4c789c5cae630580d830c0e3
renierm <renierm@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-09-25 03:50:53 +0000

    ArtifactID: 1011045 Comment: Another batch of tests for ohpi calls.

---
+++ src/t/ohpi/ohpi_011.c
@@ -0,0 +1,41 @@
+/*      -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2004
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *     Renier Morales <renierm@users.sf.net>
+ */
+ 
+#include <stdlib.h>
+#include <SaHpi.h>
+#include <oHpi.h>
+
+/**
+ * Load the plugin passin NULL as argument.
+ * Pass on error, otherwise test failed.
+ **/
+ 
+int main(int argc, char **argv)
+{
+        SaHpiSessionIdT sid = 0;
+        char *config_file = NULL;
+        
+        /* Save config file env variable and unset it */
+        config_file = getenv("OPENHPI_CONF");
+        setenv("OPENHPI_CONF","./noconfig", 1);
+        
+        if (saHpiSessionOpen(1, &sid, NULL))
+                return -1;
+                
+        /* Restore config file env variable */
+        setenv("OPENHPI_CONF",config_file,1);
+        
+        return !oHpiPluginLoad(NULL);
+}
