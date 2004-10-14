02a18b23ed202d9554bff9ee6af9cfcb52172522
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-10-14 13:43:59 +0000

    Common functions for unit tests

--- plugins/snmp_bc/t/tcontrol_parms.c
+++ plugins/snmp_bc/t/tcontrol_parms.c
@@ -18,9 +18,6 @@
 #include <sahpimacros.h>
 #include <tsetup.h>
 
-static void 
-checkstatus(SaErrorT *err, SaErrorT *expected_err, int *failed, int *pass, int *testcase, int *testfail);
-
 int main(int argc, char **argv) 
 {
 
@@ -28,8 +25,6 @@ int main(int argc, char **argv)
 	 * Local variables
 	 * ***********************/	 
 	int testfail = 0;
-	int testcase = 0;
-	int passed = 0, failed = 0;
 	SaHpiResourceIdT  id;
 	SaHpiParmActionT  act;
 	SaErrorT          err;
@@ -63,31 +58,28 @@ int main(int argc, char **argv)
 	/************************** 
 	 * Test 1: Invalid Control Action
 	 **************************/
-	testcase++;
 	id = rptentry.ResourceId;
 	expected_err = SA_ERR_HPI_INVALID_PARAMS;
 	act = 0xFF;
 																																														
 	err = snmp_bc_control_parm((void *)h->hnd, id, act);
-	checkstatus(&err, &expected_err, &failed, &passed, &testcase, &testfail);
+	checkstatus(&err, &expected_err, &testfail);
 	
 	/************************** 
 	 * Test 2: Invalid ResourceId
 	 **************************/
-	testcase++;
 	expected_err = SA_ERR_HPI_INVALID_RESOURCE;
 	act = SAHPI_RESTORE_PARM;
 	id = 5000; /* set it way out */
 
 	err = snmp_bc_control_parm((void *)h->hnd, id, act);
-	checkstatus(&err, &expected_err, &failed, &passed, &testcase, &testfail);
+	checkstatus(&err, &expected_err, &testfail);
 
 	id = rptentry.ResourceId;
 	struct oh_handler_state *handle = (struct oh_handler_state *)h->hnd;		
 	/************************** 
 	 * Test 3: 
 	 *************************/
-	testcase++;
         OH_GET_DID(sessionid, did);
 	OH_GET_DOMAIN(did, d); /* Lock domain */
 	rptentry.ResourceCapabilities |= SAHPI_CAPABILITY_CONFIGURATION;  
@@ -97,12 +89,12 @@ int main(int argc, char **argv)
 	expected_err = SA_ERR_HPI_INTERNAL_ERROR;
 
 	err = snmp_bc_control_parm((void *)h->hnd, id, act);
-	checkstatus(&err, &expected_err, &failed, &passed, &testcase, &testfail);
+	checkstatus(&err, &expected_err, &testfail);
 
 	/************************** 
 	 * Test 4: 
 	 **************************/
-	testcase++;
+
 	OH_GET_DOMAIN(did, d); /* Lock domain */
 	rptentry.ResourceCapabilities &=  !SAHPI_CAPABILITY_CONFIGURATION;
 	oh_add_resource(handle->rptcache, &rptentry, NULL, 0);
@@ -111,32 +103,14 @@ int main(int argc, char **argv)
 	expected_err = SA_ERR_HPI_CAPABILITY;
 
 	err = snmp_bc_control_parm((void *)h->hnd, id, act);
-	checkstatus(&err, &expected_err, &failed, &passed, &testcase, &testfail);
+	checkstatus(&err, &expected_err, &testfail);
 	
 	/***************************
 	 * Cleanup after all tests
 	 ***************************/
-	 printf("tcontrol_parms: %d passed, %d failed\n", passed, failed);
+
 	 err = tcleanup(&sessionid);
 	 return testfail;
-
-}
-
-/*
- *
- *
- *
- */
-static void 
-checkstatus(SaErrorT *err, SaErrorT *expected_err, int *failed, int *passed, int *testcase, int *testfail)
-{
-	if (*err != *expected_err) {
-		printf("Error! bc_get_sensor_reading: TestCase %d\n", *testcase);
-		printf("Error! snmp_bc_get_sensor_reading returned err=%s, expected=%s\n",
-		oh_lookup_error(*err), oh_lookup_error(*expected_err));
-		*testfail = -1;
-		(*failed)++;
-	} else (*passed)++;
 
 }
 
