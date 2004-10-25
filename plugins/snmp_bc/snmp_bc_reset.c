879e9d6f285b81b99c2891a0dcafd37fe9164452
sesherman <sesherman@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-10-25 01:38:17 +0000

    ArtifactId: 1053471 Comment: port reset code to B.1.1

--- plugins/snmp_bc/snmp_bc_reset.c
+++ plugins/snmp_bc/snmp_bc_reset.c
@@ -16,109 +16,134 @@
 #include <snmp_bc_plugin.h>
 
 /**
- * snmp_bc_get_hotswap_state:
+ * snmp_bc_get_reset_state:
  * @hnd: Handler data pointer.
  * @rid: Resource ID.
- * @state: Location to store resource's hotswap state.
+ * @act: Location to store resource's reset action state.
  *
- * Retrieves a sensor's value and/or state. Both @data and @state
- * may be NULL, in which case this function can be used to test for
- * sensor presence.
+ * Retrieves a resource's reset action state.
+ * Always return  SAHPI_RESET_DEASSERT.
  *
  * Return values:
  * SA_OK - Normal case.
- * SA_ERR_HPI_CAPABILITY - Resource doesn't have SAHPI_CAPABILITY_SENSOR.
+ * SA_ERR_HPI_CAPABILITY - Resource doesn't have SAHPI_CAPABILITY_RESET.
  * SA_ERR_HPI_INVALID_RESOURCE - Resource doesn't exist.
- * SA_ERR_HPI_INVALID_REQUEST - Sensor is disabled.
  * SA_ERR_HPI_INVALID_PARAMS - Pointer parameter(s) are NULL.
  **/
 SaErrorT snmp_bc_get_reset_state(void *hnd,
 				 SaHpiResourceIdT rid,
 				 SaHpiResetActionT *act)
 {
-
-	if (!hnd || !act){
-		dbg("Missing handle\n");
-		return SA_ERR_HPI_INVALID_PARAMS;
+	if (!hnd || !act) {
+		dbg("Invalid parameter.");
+		return(SA_ERR_HPI_INVALID_PARAMS);
 	}
 
         struct oh_handler_state *handle = (struct oh_handler_state *)hnd;
-	SaHpiRptEntryT *res = oh_get_resource_by_id(handle->rptcache, rid);
-	if(res == NULL) {
-		return SA_ERR_HPI_INVALID_RESOURCE;
+        struct snmp_bc_hnd *custom_handle = (struct snmp_bc_hnd *)handle->data;
+
+	if (!custom_handle) {
+		dbg("Invalid parameter.");
+		return(SA_ERR_HPI_INVALID_PARAMS);
 	}
 
-        struct ResourceInfo *s =
-                (struct ResourceInfo *)oh_get_resource_data(handle->rptcache, rid);
-	if(s == NULL) {
-		return SA_ERR_HPI_INVALID_CMD;
-	}
-	if(s->mib.OidReset == NULL) { 
-		return SA_ERR_HPI_INVALID_CMD;
-	}
+	/* Check if resource exists and has reset capabilities */
+	SaHpiRptEntryT *rpt = oh_get_resource_by_id(handle->rptcache, rid);
+        if (!rpt) return(SA_ERR_HPI_INVALID_RESOURCE);
+        if (!(rpt->ResourceCapabilities & SAHPI_CAPABILITY_RESET)) return(SA_ERR_HPI_CAPABILITY);
 
 	*act = SAHPI_RESET_DEASSERT;
 
-	return SA_OK;
+	return(SA_OK);
 }
 
+/**
+ * snmp_bc_set_reset_state:
+ * @hnd: Handler data pointer.
+ * @rid: Resource ID.
+ * @act: Reset action state to set.
+ *
+ * Sets a resource's reset action state.
+ *
+ * Return values:
+ * SA_OK - Normal case.
+ * SA_ERR_HPI_CAPABILITY - Resource doesn't have SAHPI_CAPABILITY_RESET.
+ * SA_ERR_HPI_INVALID_REQUEST - @act invalid or SAHPI_RESET_ASSERT.
+ * SA_ERR_HPI_INVALID_RESOURCE - Resource doesn't exist.
+ * SA_ERR_HPI_INVALID_PARAMS - Pointer parameter(s) are NULL.
+ **/
 SaErrorT snmp_bc_set_reset_state(void *hnd,
 				 SaHpiResourceIdT rid,
 				 SaHpiResetActionT act)
 {
+	gchar *oid;
+	SaErrorT err;
+	struct ResourceInfo *resinfo;
+        struct snmp_value set_value;
+
 	if (!hnd){
-		dbg("Missing handle\n");
+		dbg("Invalid parameter.");
 		return SA_ERR_HPI_INVALID_PARAMS;
 	}
 
-	gchar *oid;
-	SaErrorT status;
-        struct snmp_value set_value;
+	if (NULL == oh_lookup_resetaction(act) || act == SAHPI_RESET_ASSERT) {
+		dbg("Invalid reset action.");
+		return SA_ERR_HPI_INVALID_REQUEST;
+	}
+	
         struct oh_handler_state *handle = (struct oh_handler_state *)hnd;
         struct snmp_bc_hnd *custom_handle = (struct snmp_bc_hnd *)handle->data;
 
-        SaHpiRptEntryT *res = oh_get_resource_by_id(handle->rptcache, rid);
-	if(res == NULL) {
-		return SA_ERR_HPI_INVALID_RESOURCE;
+	if (!custom_handle) {
+		dbg("Invalid parameter.");
+		return(SA_ERR_HPI_INVALID_PARAMS);
 	}
-        struct ResourceInfo *s =
-                (struct ResourceInfo *)oh_get_resource_data(handle->rptcache, rid);
-	if(s == NULL) {
-		return SA_ERR_HPI_INVALID_CMD;
-	}
-	if(s->mib.OidReset == NULL) { 
-		return SA_ERR_HPI_INVALID_CMD;
+
+	/* Check if resource exists and has reset capabilities */
+	SaHpiRptEntryT *rpt = oh_get_resource_by_id(handle->rptcache, rid);
+        if (!rpt) return(SA_ERR_HPI_INVALID_RESOURCE);
+        if (!(rpt->ResourceCapabilities & SAHPI_CAPABILITY_RESET)) return(SA_ERR_HPI_CAPABILITY);
+
+	resinfo = (struct ResourceInfo *)oh_get_resource_data(handle->rptcache, rid);
+ 	if (resinfo == NULL) {
+		dbg("No resource data. Resource=%s", rpt->ResourceTag.Data);
+		return(SA_ERR_HPI_INTERNAL_ERROR);
+	}       
+
+	if (resinfo->mib.OidReset == NULL) {
+		dbg("No Reset OID.");
+		return(SA_ERR_HPI_INTERNAL_ERROR);
 	}
 
 	switch (act) {
 	case SAHPI_RESET_ASSERT: /* RESET_ASSERT = RESET_DEASSERT Action */
 	case SAHPI_RESET_DEASSERT:
-		return SA_ERR_HPI_INVALID_CMD;
+		return(SA_ERR_HPI_INVALID_REQUEST);
 	case SAHPI_COLD_RESET: /* COLD = WARM Reset Action */
 	case SAHPI_WARM_RESET:
-		oid = oh_derive_string(&(res->ResourceEntity), s->mib.OidReset);
-		if(oid == NULL) {
-			dbg("NULL SNMP OID returned for %s\n",s->mib.OidReset);
-			return SA_ERR_HPI_INTERNAL_ERROR;
+		oid = oh_derive_string(&(rpt->ResourceEntity), resinfo->mib.OidReset);
+		if (oid == NULL) {
+			dbg("NULL SNMP OID returned for %s", resinfo->mib.OidReset);
+			return(SA_ERR_HPI_INTERNAL_ERROR);
 		}
 		
 		set_value.type = ASN_INTEGER;
 		set_value.str_len = 1;
 		set_value.integer = 1;
 		
-		status = snmp_bc_snmp_set(custom_handle, oid, set_value);
-		if (status != SA_OK) {
-			dbg("SNMP could not set %s; Type=%d.\n",oid,set_value.type);
+		err = snmp_bc_snmp_set(custom_handle, oid, set_value);
+		if (err) {
+			dbg("SNMP could not set %s; Type=%d.", oid, set_value.type);
 			g_free(oid);
-			if (status == SA_ERR_HPI_BUSY) return status;
-			else return SA_ERR_HPI_NO_RESPONSE;
+			if (err == SA_ERR_HPI_BUSY) return(err);
+			else return(SA_ERR_HPI_NO_RESPONSE);
 		}
 		g_free(oid);
 		break;
 	default:
-		dbg("Invalid Reset Action Type - %d\n", act);
-		return SA_ERR_HPI_INVALID_PARAMS;
+		dbg("Invalid Reset Action Type - %d", act);
+		return(SA_ERR_HPI_INTERNAL_ERROR);
 	}
 
-        return SA_OK;
+        return(SA_OK);
 }
