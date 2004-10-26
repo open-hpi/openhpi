789934a111a1e479c3bc92643ca8e793bb18d363
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-10-26 16:22:58 +0000

    Use B-spec datatype

--- plugins/snmp_bc/t/thotswap002.c
+++ plugins/snmp_bc/t/thotswap002.c
@@ -30,7 +30,7 @@ int main(int argc, char **argv)
 					
 	SaHpiResourceIdT  id = 0;
         SaHpiSessionIdT sessionid;
-	SaHpiHsIndicatorStateT state = 0;
+	SaHpiHsStateT state = 0;
         /* *************************************                 
 	 * Find a resource 
 	 * * ************************************* */
