fe4aa81d068fe9afbd40992f666811e22041c0e3
tariqx <tariqx@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2004-12-09 22:40:35 +0000

    ArtifactID: Comment: Tied OpenIPMI Debug info to OpenHPI's Debug flag

--- plugins/ipmi/posix_vlog.c
+++ plugins/ipmi/posix_vlog.c
@@ -17,39 +17,46 @@
 void
 posix_vlog(char *format, enum ipmi_log_type_e log_type, va_list ap)
 {
-    int do_nl = 1;
-
+     int do_nl = 1;
+     char *dstr = getenv("OPENHPI_DEBUG");
+     
+     if (((log_type <= IPMI_LOG_WARNING) || (log_type == IPMI_LOG_ERR_INFO))
+		     && (!dstr || !strcmp("YES",getenv("OPENHPI_DEBUG"))))
+	     return;
     switch(log_type)
     {
 	case IPMI_LOG_INFO:
 	    printf("INFO: ");
+	    do_nl = 0;
 	    break;
 
 	case IPMI_LOG_WARNING:
 	    printf("WARN: ");
+	    do_nl = 0;
 	    break;
 
 	case IPMI_LOG_SEVERE:
 	    printf("SEVR: ");
+	    do_nl = 0;
 	    break;
 
 	case IPMI_LOG_FATAL:
 	    printf("FATL: ");
+	    do_nl = 0;
 	    break;
 
 	case IPMI_LOG_ERR_INFO:
 	    printf("EINF: ");
+	    do_nl = 1;
 	    break;
 
 	case IPMI_LOG_DEBUG_START:
-	    do_nl = 0;
 	    /* FALLTHROUGH */
 	case IPMI_LOG_DEBUG:
 	    printf("DEBG: ");
 	    break;
 
 	case IPMI_LOG_DEBUG_CONT:
-	    do_nl = 0;
 	    /* FALLTHROUGH */
 	case IPMI_LOG_DEBUG_END:
 	    break;
