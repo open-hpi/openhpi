8e44c3a1f3e5dcc3c9168a4757e614a65ad31e3a
kouzmich <kouzmich@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-01-13 10:36:12 +0000

    hpi_ui_lib update

--- include/hpi_ui.h
+++ include/hpi_ui.h
@@ -112,7 +112,7 @@ extern SaErrorT	show_sensor_list(SaHpiSessionIdT sessionid, SaHpiResourceIdT res
 			hpi_ui_print_cb_t proc);
 extern SaErrorT	show_sensor(SaHpiSessionIdT sessionid, SaHpiResourceIdT resourceid,
 			SaHpiSensorNumT sensornum, hpi_ui_print_cb_t proc);
-extern void	show_short_event(SaHpiEventT *event);
+extern void	show_short_event(SaHpiEventT *event, hpi_ui_print_cb_t proc);
 extern SaErrorT	show_threshold(SaHpiSessionIdT sessionid, SaHpiResourceIdT resourceid,
 			SaHpiSensorNumT sensornum, hpi_ui_print_cb_t proc);
 extern int	print_thres_value(SaHpiSensorReadingT *item, char *mes,
