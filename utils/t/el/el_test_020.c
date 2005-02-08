1dc5bb645fa2078a6fb7b68e95ca65cb0c73745b
chernandez_ibm <chernandez_ibm@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-02-08 22:40:23 +0000

    Artifact: 12345 Comment: adding event log test cases

--- utils/t/el/el_test_020.c
+++ utils/t/el/el_test_020.c
@@ -31,10 +31,9 @@
  * main: EL test
  *
  * This test verifies failure of oh_el_append when (1) el == NULL,
- * (2) event == NULL,(3) el->enabled == SAHPI_FALSE &&
- * event->EventType == SAHPI_ET_USER, (4) el->enabled == SAHPI_FALSE &&
- * event->EventType != SAHPI_ET_USER, (5) entry = NULL, (6) el->maxsize !=
- * OH_EL_MAX_SIZE && g_list_length(el->elentries) == el->maxsize, and (7)
+ * (2) event == NULL, (3) el->enabled == SAHPI_FALSE &&
+ * event->EventType != SAHPI_ET_USER, (4) entry = NULL, (5) el->maxsize !=
+ * OH_EL_MAX_SIZE && g_list_length(el->elentries) == el->maxsize, and (6)
  * el-> gentimestamp == FALSE
  *
  * Return value: 0 on success, 1 on failure
@@ -43,11 +42,9 @@
 
 int main(int argc, char **argv)
 {
-        oh_el *el, *el2, *el3, *el4, *el5, *el6, *el7;
+        oh_el *el, *el2, *el3, *el4, *el5, *el6;
         SaErrorT retc;
-	int x;
-	SaHpiEventT event, event2, event3, event4, event5, event6;
-	oh_el_entry *entry;
+	SaHpiEventT event, event2, event3, event4, event5;
 	static char *data[1] = {
         	"Test data one"
 
@@ -73,45 +70,26 @@ int main(int argc, char **argv)
 
 	/*test oh_el_append with event==NULL*/
 	el2 = oh_el_create(20);
-        retc = oh_el_append(el, NULL, NULL, NULL);
+        retc = oh_el_append(el2, NULL, NULL, NULL);
         if (retc == SA_OK) {
                 dbg("ERROR: oh_el_append failed.");
                 return 1;
         } 
-
+	
 	/*test oh_el_append with el->enabled == SAHPI_FALSE &&
-        event->EventType == SAHPI_ET_USER */
+        event->EventType != SAHPI_ET_USER */
 	
 	el3 = oh_el_create(20);
 
 	el3->enabled = FALSE;
         event2.Source = 1;
-        event2.EventType = SAHPI_ET_USER;
+        event2.EventType = SAHPI_ET_DOMAIN;
         event2.Timestamp = SAHPI_TIME_UNSPECIFIED;
         event2.Severity = SAHPI_DEBUG;
 
         strcpy((char *) &event2.EventDataUnion.UserEvent.UserEventData.Data, data[0]);
 
-        retc = oh_el_append(el, &event2, NULL, NULL);
-        if (retc == SA_OK) {
-                dbg("ERROR: oh_el_append failed.");
-                return 1;
-        }   
-	
-	/*test oh_el_append with el->enabled == SAHPI_FALSE &&
-        event->EventType != SAHPI_ET_USER */
-	
-	el4 = oh_el_create(20);
-
-	el4->enabled = FALSE;
-        event3.Source = 1;
-        event3.EventType = SAHPI_ET_DOMAIN;
-        event3.Timestamp = SAHPI_TIME_UNSPECIFIED;
-        event3.Severity = SAHPI_DEBUG;
-
-        strcpy((char *) &event3.EventDataUnion.UserEvent.UserEventData.Data, data[0]);
-
-        retc = oh_el_append(el, &event3, NULL, NULL);
+        retc = oh_el_append(el3, &event2, NULL, NULL);
         if (retc == SA_OK) {
                 dbg("ERROR: oh_el_append failed.");
                 return 1;
@@ -119,56 +97,50 @@ int main(int argc, char **argv)
 	
 	/*test oh_el_append with entry == NULL */
 	
-	el5 = oh_el_create(20);
+	el4 = oh_el_create(0);
 
+        event3.Source = 1;
+        event3.EventType = SAHPI_ET_USER;
+        event3.Timestamp = SAHPI_TIME_UNSPECIFIED;
+        event3.Severity = SAHPI_DEBUG;
+	strcpy((char *) &event3.EventDataUnion.UserEvent.UserEventData.Data, data[0]);
+        retc = oh_el_append(el4, &event3, NULL, NULL);
+	
+        if (retc != SA_OK) {
+                dbg("ERROR: oh_el_append failed.");
+                return 1;
+        }  
+	
+	/*test oh_el_append with el->maxsize != OH_EL_MAX_SIZE && g_list_length(el->elentries) == el->maxsize */
+	
+	el5 = oh_el_create(20);
+	el5->maxsize = g_list_length(el5->elentries);
         event4.Source = 1;
         event4.EventType = SAHPI_ET_USER;
         event4.Timestamp = SAHPI_TIME_UNSPECIFIED;
         event4.Severity = SAHPI_DEBUG;
 
         strcpy((char *) &event4.EventDataUnion.UserEvent.UserEventData.Data, data[0]);
-	  entry = NULL;
 
-        retc = oh_el_append(el, &event4, NULL, NULL);
-        if (retc == SA_OK) {
+        retc = oh_el_append(el5, &event4, NULL, NULL);
+        if (retc != SA_OK) {
                 dbg("ERROR: oh_el_append failed.");
                 return 1;
-        }  
-
-	/*test oh_el_append with el->maxsize != OH_EL_MAX_SIZE && g_list_length(el->elentries) == el->maxsize */
+        } 
+	/*test oh_el_append with el->gentimestamp == FALSE*/
 	
 	el6 = oh_el_create(20);
-	x = g_list_length(el6->elentries);
-	
-	el6->maxsize = x;
 
         event5.Source = 1;
         event5.EventType = SAHPI_ET_USER;
         event5.Timestamp = SAHPI_TIME_UNSPECIFIED;
         event5.Severity = SAHPI_DEBUG;
+	  el6->gentimestamp = SAHPI_FALSE;
 
         strcpy((char *) &event5.EventDataUnion.UserEvent.UserEventData.Data, data[0]);
 
-        retc = oh_el_append(el, &event5, NULL, NULL);
-        if (retc == SA_OK) {
-                dbg("ERROR: oh_el_append failed.");
-                return 1;
-        } 
-
-	/*test oh_el_append with el->gentimestamp == FALSE*/
-	
-	el7 = oh_el_create(20);
-
-        event6.Source = 1;
-        event6.EventType = SAHPI_ET_USER;
-        event6.Timestamp = SAHPI_TIME_UNSPECIFIED;
-        event6.Severity = SAHPI_DEBUG;
-	  el7->gentimestamp = FALSE;
-
-        strcpy((char *) &event6.EventDataUnion.UserEvent.UserEventData.Data, data[0]);
-
-        retc = oh_el_append(el, &event6, NULL, NULL);
-        if (retc == SA_OK) {
+        retc = oh_el_append(el6, &event5, NULL, NULL);
+        if (retc != SA_OK) {
                 dbg("ERROR: oh_el_append failed.");
                 return 1;
         }  
@@ -206,15 +178,7 @@ int main(int argc, char **argv)
         if (retc != SA_OK) {
                 dbg("ERROR: oh_el_close on el6 failed.");
                 return 1;
-        }
-
-	/* close el7 */
-        retc = oh_el_close(el7);
-        if (retc != SA_OK) {
-                dbg("ERROR: oh_el_close on el7 failed.");
-                return 1;
-        }
-
+       }
 
 
         return 0;
