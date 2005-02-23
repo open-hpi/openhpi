3635a8405c893a6f0fe86130a5e20d9ff2e8ec23
sdague <sdague@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-02-23 16:40:55 +0000

    ArtifactID: Comment: removal of old simulator code

--- openhpi/trunk/plugins/simulator/sim_sensor.h
+++
@@ -1,7 +0,0 @@
-#ifndef _INC_SENSOR_H_
-#define _INC_SENSOR_H_
-
-int sim_sensor_update(struct oh_handler_state *inst,
-                      SaHpiResourceIdT rid,
-                      SaHpiSensorNumT num);
-#endif
