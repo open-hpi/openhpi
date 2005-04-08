f91ca3083df5577042d1ca594cd95378fda6f51d
sdague <sdague@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-04-08 13:02:10 +0000

    ArtifactID: Comment: check in sanity test case

---
+++ plugins/simulator/t/sim_sanity_000.c
@@ -0,0 +1,91 @@
+/*      -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2005
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Authors:
+ *     Sean Dague <http://dague.net/sean>
+ */
+ 
+#include <stdlib.h>
+#include <SaHpi.h>
+#include <oh_utils.h>
+
+/**
+ * Run a series of sanity tests on the simulator
+ * Pass on success, otherwise a failure.
+ **/
+
+/**
+ * Utility macro to make it easier to state what failed
+ */
+
+#define failed(err)                                \
+        do {                                            \
+                failcount++;                            \
+                dbg("Failed Test %d: %s", testnum, err);    \
+        } while(0) 
+
+#define runtest() testnum++
+
+int main(int argc, char **argv)
+{
+        SaHpiSessionIdT sid = 0;
+        SaHpiRptEntryT res;
+        SaHpiEntryIdT id = SAHPI_FIRST_ENTRY;
+        int failcount = 0;
+        int testnum = 0;
+        SaErrorT rc = SA_OK;
+                
+        rc = saHpiSessionOpen(SAHPI_UNSPECIFIED_DOMAIN_ID, &sid, NULL);
+        runtest();
+        if(rc != SA_OK) {
+                failed("Failed to open session");
+        }
+
+        rc = saHpiDiscover(sid);
+        runtest();
+        if(rc != SA_OK) {
+                failed("Failed to run discover");
+        }
+        
+        rc = saHpiRptEntryGet(sid, id, &id, &res);
+        runtest();
+        if(rc != SA_OK) {
+                dbg("Error %s",oh_lookup_error(rc));
+                failed("Couldn't get the first rpt entry");
+                /* we're toast, bail */
+                goto end; 
+        }
+        
+        /* reset so we can do a full loop */
+        id = SAHPI_FIRST_ENTRY;
+
+        /* loop over all resources, ensure that ResourceTag and 
+           ManufacturerId have been set */
+        while(saHpiRptEntryGet(sid,id,&id,&res) == SA_OK) {
+                runtest();
+                if(!res.ResourceTag.DataLength) {
+                        failed("Resource Tag has zero length");
+                }
+                runtest();
+                if(!res.ResourceInfo.ManufacturerId) {
+                        failed("Resource has no Manufacturer Id");
+                }
+
+                /* there should be an inner loop here for Rdrs */
+        }
+        dbg("Ran %d tests", testnum);
+        /* if there is any failures, the test fails */
+end:
+        if(failcount) {
+                return -1;
+        }
+	return(0);
+}
