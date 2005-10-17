d4372f15b9d390c0b36d5c184fe66835791e57ab
wdashley <wdashley@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-10-17 21:01:04 +0000

    ArtifactID: 1208131 Comment: More struct classes.

---
+++ openhpi/trunk/cpp/oSaHpiCtrlRecStream.hpp
@@ -0,0 +1,43 @@
+/*      -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2005
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Author(s):
+ *    W. David Ashley <dashley@us.ibm.com>
+ */
+
+
+#ifndef Included_oSaHpiCtrlRecStream
+#define Included_oSaHpiCtrlRecStream
+
+#include <stdio.h>
+extern "C"
+{
+#include <SaHpi.h>
+}
+
+
+class oSaHpiCtrlRecStream : public SaHpiCtrlRecStreamT {
+    public:
+        // constructors
+        oSaHpiCtrlRecStream();
+        // copy constructor
+        oSaHpiCtrlRecStream(const oSaHpiCtrlRecStream& crd);
+        // destructor
+        ~oSaHpiCtrlRecStream() {
+        }
+        // other methods
+        inline SaHpiCtrlRecStreamT *getStruct(void) {
+            return this;
+        }
+};
+
+#endif
+
