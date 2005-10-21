e9f0649dcd479391dd4d08b12e4dc48b3d041b59
renierm <renierm@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-10-21 02:04:49 +0000

    ArtifactID: 1333708 Comment: Initial creation of rtas plugin files

---
+++ plugins/rtas/oh_rtas.c
@@ -0,0 +1,18 @@
+/*      -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2005
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Author(s):
+ *        Renier Morales <renierm@users.sf.net>
+ */
+
+#include <oh_rtas.h>
+
+
---
+++ plugins/rtas/oh_rtas.h
@@ -0,0 +1,26 @@
+/*      -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2005
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Author(s):
+ *        Renier Morales <renierm@users.sf.net>
+ */
+
+#ifndef _OH_RTAS_H
+#define _OH_RTAS_H
+
+#include <glib.h>
+#include <SaHpi.h>
+#include <oh_handler.h>
+#include <oh_utils.h>
+#include <oh_error.h>
+
+#endif /* _OH_RTAS_H */
+
