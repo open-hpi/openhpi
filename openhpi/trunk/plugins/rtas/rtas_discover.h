534287530e60206f33efde15fc37fafe460f27af
renierm <renierm@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2006-04-05 08:16:19 +0000

    ArtifactID: 1333708 Comment: Adding inventory, power, and resource abi implementations

--- openhpi/trunk/plugins/rtas/rtas_discover.h
+++ openhpi/trunk/plugins/rtas/rtas_discover.h
@@ -13,7 +13,7 @@
  *        Renier Morales <renierm@users.sf.net>
  *        Daniel de Araujo <ddearauj@us.ibm.com>
  */
- 
+
 #ifndef RTAS_DISCOVER_H
 #define RTAS_DISCOVER_H
 
@@ -26,43 +26,47 @@
 #include <rtas_sensor.h>
 #include <librtas.h>
 
+#define LSVPD_CMD "/sbin/lsvpd"
 
 /* Enums */
 typedef enum rtasSensorTokenEnum {
-	RTAS_SECURITY_SENSOR = 1,
-	RTAS_RESERVED_SENSOR_2,
-	RTAS_THERMAL_SENSOR,
-	RTAS_RESERVED_SENSOR_4,
-	RTAS_RESERVED_SENSOR_5,
-	RTAS_RESERVED_SENSOR_6,
-	RTAS_RESERVED_SENSOR_7,
-	RTAS_RESERVED_SENSOR_8,
-	RTAS_POWER_STATE_SENSOR,
-	RTAS_RESERVED_SENSOR_10,
-	RTAS_RESERVED_SENSOR_11,
-	RTAS_SURVEILLANCE_SENSOR = 9000,
-	RTAS_FAN_SENSOR,
-	RTAS_VOLTAGE_SENSOR,
-	RTAS_CONNECTOR_SENSOR,
-	RTAS_POWER_SUPPLY_SENSOR,
-	RTAS_GIQ_SENSOR,
-	RTAS_SYSTEM_ATTENTION_SENSOR,
-	RTAS_IDENTIFY_INDICATOR_SENSOR,
-	RTAS_RESERVED_SENSOR_9008,
-	RTAS_COMPONENT_RESET_STATE_SENSOR,
-	RTAS_OEM_SPECIFIC_SENSOR_START,
-	RTAS_OEM_SPECIFIC_SENSOR_END = 9999
-}rtasSensorToken;
-	
-/* Function Protos */	
+        RTAS_SECURITY_SENSOR = 1,
+        RTAS_RESERVED_SENSOR_2,
+        RTAS_THERMAL_SENSOR,
+        RTAS_RESERVED_SENSOR_4,
+        RTAS_RESERVED_SENSOR_5,
+        RTAS_RESERVED_SENSOR_6,
+        RTAS_RESERVED_SENSOR_7,
+        RTAS_RESERVED_SENSOR_8,
+        RTAS_POWER_STATE_SENSOR,
+        RTAS_RESERVED_SENSOR_10,
+        RTAS_RESERVED_SENSOR_11,
+        RTAS_SURVEILLANCE_SENSOR = 9000,
+        RTAS_FAN_SENSOR,
+        RTAS_VOLTAGE_SENSOR,
+        RTAS_CONNECTOR_SENSOR,
+        RTAS_POWER_SUPPLY_SENSOR,
+        RTAS_GIQ_SENSOR,
+        RTAS_SYSTEM_ATTENTION_SENSOR,
+        RTAS_IDENTIFY_INDICATOR_SENSOR,
+        RTAS_RESERVED_SENSOR_9008,
+        RTAS_COMPONENT_RESET_STATE_SENSOR,
+        RTAS_OEM_SPECIFIC_SENSOR_START,
+        RTAS_OEM_SPECIFIC_SENSOR_END = 9999
+} rtasSensorToken;
+
+/* Function Protos */
 SaErrorT rtas_discover_resources(void *hnd);
 
 SaErrorT rtas_discover_domain_resources(void *hnd, SaHpiDomainIdT did);
 
 SaErrorT rtas_discover_sensors(struct oh_handler_state *handle,
-			       struct oh_event *res_oh_event);
-			       
+                               struct oh_event *res_oh_event);
+
+SaErrorT rtas_discover_inventory(struct oh_handler_state *handle,
+                                 struct oh_event *res_oh_event);
+
 void populate_rtas_sensor_rec_info(int token, SaHpiSensorRecT *sensor_info);
 
-			       			       
+
 #endif /* RTAS_DISCOVER_H */
--- openhpi/trunk/plugins/rtas/rtas_resource.c
+++ openhpi/trunk/plugins/rtas/rtas_resource.c
@@ -15,19 +15,59 @@
  */
 
 #include <rtas_resource.h>
+#include <oh_handler.h>
+#include <oh_event.h>
+#include <oh_domain.h>
+#include <oh_utils.h>
 
 SaErrorT rtas_set_resource_tag(void *hnd,
-                                  SaHpiResourceIdT id,
-                                  SaHpiTextBufferT *tag)
+                               SaHpiResourceIdT id,
+                               SaHpiTextBufferT *tag)
 {
-        return SA_ERR_HPI_INTERNAL_ERROR;
+        struct oh_handler_state *h = hnd;
+        SaHpiRptEntryT *rptentry = NULL;
+        SaErrorT error = SA_OK;
+        struct oh_event *e = NULL;
+
+        if (!hnd) return SA_ERR_HPI_INTERNAL_ERROR;
+
+        rptentry = oh_get_resource_by_id(h->rptcache, id);
+        if (!rptentry) return SA_ERR_HPI_NOT_PRESENT;
+
+        error = oh_append_textbuffer(&rptentry->ResourceTag, tag->Data);
+        if (error) return error;
+
+        e = (struct oh_event *)g_malloc0(sizeof(struct oh_event));
+        e->did = oh_get_default_domain_id();
+        e->type = OH_ET_RESOURCE;
+        e->u.res_event.entry = *rptentry;
+        h->eventq = g_slist_append(h->eventq, e);
+
+        return SA_OK;
 }
 
 SaErrorT rtas_set_resource_severity(void *hnd,
-                                       SaHpiResourceIdT id,
-                                       SaHpiSeverityT sev)
+                                    SaHpiResourceIdT id,
+                                    SaHpiSeverityT sev)
 {
-        return SA_ERR_HPI_INTERNAL_ERROR;
+        struct oh_handler_state *h = hnd;
+        SaHpiRptEntryT *rptentry = NULL;
+        struct oh_event *e = NULL;
+
+        if (!hnd) return SA_ERR_HPI_INTERNAL_ERROR;
+
+        rptentry = oh_get_resource_by_id(h->rptcache, id);
+        if (!rptentry) return SA_ERR_HPI_NOT_PRESENT;
+
+        rptentry->ResourceSeverity = sev;
+
+        e = (struct oh_event *)g_malloc0(sizeof(struct oh_event));
+        e->did = oh_get_default_domain_id();
+        e->type = OH_ET_RESOURCE;
+        e->u.res_event.entry = *rptentry;
+        h->eventq = g_slist_append(h->eventq, e);
+
+        return SA_OK;
 }
 
 
