5e6a3f77982e4de376cb8c159191078b2ffffc11
djudkovi <djudkovi@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2006-04-07 19:27:13 +0000

    ArtifactID: Comment:

--- include/oh_config.h
+++ include/oh_config.h
@@ -42,6 +42,7 @@ typedef enum {
         OPENHPI_DEL_SAVE,
         OPENHPI_DAT_SIZE_LIMIT,
         OPENHPI_DAT_USER_LIMIT,
+        OPENHPI_DAEMON_MODE,
         //OPENHPI_DEBUG,
         //OPENHPI_DEBUG_TRACE,
         //OPENHPI_DEBUG_LOCK,
@@ -59,6 +60,7 @@ typedef union {
         SaHpiBoolT del_save;
         SaHpiUint32T dat_size_limit;
         SaHpiUint32T dat_user_limit;
+        SaHpiUint32T daemon_mode;
         //unsigned char dbg; /* 1 = YES, 0 = NO */
         //unsigned char dbg_trace; /* !0 = YES, 0 = NO */
         //unsigned char dbg_lock; /* !0 = YES, 0 = NO */
