fec8a19a4fc80dbfbf60a5e0c62d1582e29949cf
renierm <renierm@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2006-07-11 23:14:44 +0000

    #1491791 Adding alarm table presistency

--- openhpi/trunk/include/oHpi.h
+++ openhpi/trunk/include/oHpi.h
@@ -41,6 +41,7 @@ typedef enum {
         OHPI_DEL_SAVE,
         OHPI_DAT_SIZE_LIMIT,
         OHPI_DAT_USER_LIMIT,
+	OHPI_DAT_SAVE,
         OHPI_DAEMON_MODE,
         //OHPI_DEBUG,
         //OHPI_DEBUG_TRACE,
@@ -59,6 +60,7 @@ typedef union {
         SaHpiBoolT DelSave; /* True if domain event log is to be saved to disk */
         SaHpiUint32T DatSizeLimit; /* Max alarms allowed in alarm table */
         SaHpiUint32T DatUserLimit; /* Max number of user alarms allowed */
+	SaHpiBoolT DatSave; /* True if domain alarm table is to be saved to disk */
         SaHpiUint32T Daemon; /* 1 = YES, 0 = NO */
         //unsigned char Debug; /* 1 = YES, 0 = NO */
         //unsigned char DebugTrace; /* !0 = YES, 0 = NO */
