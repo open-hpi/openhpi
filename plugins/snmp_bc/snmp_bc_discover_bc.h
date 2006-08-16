576938c8ce7034c1a201ca028fb30a54fd20e78d
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2006-08-16 16:11:17 +0000

    Re structure snmp_bc to use new oh_event definition, Feature 1541402

--- plugins/snmp_bc/snmp_bc_discover_bc.h
+++ plugins/snmp_bc/snmp_bc_discover_bc.h
@@ -21,6 +21,9 @@
 #define SNMP_BC_IPMI_TEMP_BLADE_OID ".1.3.6.1.4.1.2.3.51.2.22.1.5.3.1.11.x"
 #define SNMP_BC_IPMI_VOLTAGE_BLADE_OID ".1.3.6.1.4.1.2.3.51.2.22.1.5.5.1.14.x"
 
+#define SNMP_BC_RESOURCE_INSTALLED 1
+#define SNMP_BC_RESOURCE_REMOVED   2
+
 #define get_installed_mask(maskOID, getvalue) \
 do { \
 	err = snmp_bc_snmp_get(custom_handle, maskOID, &getvalue, SAHPI_TRUE); \
@@ -45,7 +48,88 @@ do { \
         } \
 } while(0)
 
+#define pdp_debug(string) \
+do { \
+	err = snmp_bc_snmp_get(custom_handle, maskOID, &getintvalue, SAHPI_TRUE); \
+        if (err || getintvalue.type != ASN_INTEGER) { \
+		dbg("Cannot get OID=%s; Received Type=%d; Error=%s.", \
+		      		maskOID, getintvalue.type, oh_lookup_error(err)); \
+		if (err) { return(err); } \
+		else { return(SA_ERR_HPI_INTERNAL_ERROR); } \
+        } \
+} while(0)
 
+guint snmp_bc_isrediscover(SaHpiEventT *working_event);
+
+SaErrorT snmp_bc_construct_blade_rpt(struct oh_event *e, 
+				     struct ResourceInfo **res_info_ptr,
+				     SaHpiEntityPathT *ep_root, 
+				     guint blade_index);
+				     
+SaErrorT snmp_bc_construct_blower_rpt(struct oh_event* e, 
+				      struct ResourceInfo **res_info_ptr,
+				      SaHpiEntityPathT *ep_root, 
+				      guint blower_index);
+				      
+SaErrorT snmp_bc_construct_pm_rpt(struct oh_event *e, 
+				  struct ResourceInfo **res_info_ptr, 
+				  SaHpiEntityPathT *ep_root,
+				  guint pm_index);
+				  
+SaErrorT snmp_bc_construct_sm_rpt(struct oh_event *e, 
+				  struct ResourceInfo **res_info_ptr,
+				  SaHpiEntityPathT *ep_root, 
+				  guint sm_index);
+				  
+SaErrorT snmp_bc_construct_mm_rpt(struct oh_event *e, 
+				  struct ResourceInfo **res_info_ptr,
+				  SaHpiEntityPathT *ep_root, 
+				  guint mm_index);
+				  
+SaErrorT snmp_bc_add_blade_rptcache(struct oh_handler_state *handle, 
+				  struct oh_event *e, 
+				  struct ResourceInfo *res_info_ptr,
+				  guint blade_index);
+				  
+SaErrorT snmp_bc_add_blower_rptcache(struct oh_handler_state *handle, 
+				  struct oh_event *e, 
+				  struct ResourceInfo *res_info_ptr,
+				  guint blower_index);
+				  
+SaErrorT snmp_bc_add_pm_rptcache(struct oh_handler_state *handle, 
+				  struct oh_event *e, 
+				  struct ResourceInfo *res_info_ptr,
+				  guint pm_index);
+				  
+SaErrorT snmp_bc_add_switch_rptcache(struct oh_handler_state *handle, 
+				  struct oh_event *e, 
+				  struct ResourceInfo *res_info_ptr,
+				  guint switch_index);
+				  
+SaErrorT snmp_bc_add_mm_rptcache(struct oh_handler_state *handle, 
+				  struct oh_event *e, 
+				  struct ResourceInfo *res_info_ptr,
+				  guint mm_index);
+				  
+SaErrorT snmp_bc_discover_blade_i(struct oh_handler_state *handle,
+			  	  SaHpiEntityPathT *ep_root, 
+				  guint blade_index);
+				  
+SaErrorT snmp_bc_discover_blower_i(struct oh_handler_state *handle,
+			  	   SaHpiEntityPathT *ep_root, 
+				   guint blower_index);
+				   
+SaErrorT snmp_bc_discover_pm_i(struct oh_handler_state *handle,
+			       SaHpiEntityPathT *ep_root, 
+			       guint pm_index);
+			       
+SaErrorT snmp_bc_discover_switch_i(struct oh_handler_state *handle,
+			  	   SaHpiEntityPathT *ep_root, 
+				   guint sm_index);
+				   
+SaErrorT snmp_bc_discover_mm_i(struct oh_handler_state *handle,
+			  	SaHpiEntityPathT *ep_root, 
+				guint mm_index);
 
 #endif
 
