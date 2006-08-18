d1d2f41eeea845669f46467d44530a443ad5e193
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2006-08-18 13:19:37 +0000

    Enhance hotswap targeted (re) discovery, feature 1542620

--- openhpi/trunk/plugins/snmp_bc/snmp_bc_discover.h
+++ openhpi/trunk/plugins/snmp_bc/snmp_bc_discover.h
@@ -100,10 +100,59 @@ SaErrorT snmp_bc_add_blade_expansion_resource(struct oh_handler_state *handle,
 						guint blade_index,
 						BCExpansionTypeT expansionType,
 						guint expansionindex);
-						
-						
-						
-			       
+
+SaErrorT snmp_bc_discover_blade_i(struct oh_handler_state *handle,
+			  	  struct ResourceInfo *res_info_ptr, 
+				  struct oh_event *e,
+				  SaHpiEntityPathT *ep_root, 
+				  guint blade_index);
+			
+SaErrorT snmp_bc_discover_blower_i(struct oh_handler_state *handle,
+			  	  struct ResourceInfo *res_info_ptr, 
+				  struct oh_event *e, 
+				  guint blower_index);
+				  
+SaErrorT snmp_bc_discover_pm_i(struct oh_handler_state *handle,
+			  	  struct ResourceInfo *res_info_ptr, 
+				  struct oh_event *e, 
+				  guint pm_index);
+				  
+SaErrorT snmp_bc_discover_switch_i(struct oh_handler_state *handle,
+			  	  struct ResourceInfo *res_info_ptr, 
+				  struct oh_event *e, 
+				  guint sm_index);
+				  
+SaErrorT snmp_bc_discover_mm_i(struct oh_handler_state *handle,
+			  	  struct ResourceInfo *res_info_ptr, 
+				  struct oh_event *e, 
+				  guint mm_index);
+				  
+SaErrorT snmp_bc_construct_blade_res(struct oh_event **e,
+			  	     struct ResourceInfo **res_info_ptr,
+				     SaHpiEntityPathT *ep_root, 
+				     guint blade_index);
+				     
+SaErrorT snmp_bc_construct_blower_res(struct oh_event **e,
+			  	     struct ResourceInfo **res_info_ptr,
+				     SaHpiEntityPathT *ep_root, 
+				     guint blower_index);
+				   
+				  
+SaErrorT snmp_bc_construct_pm_res(struct oh_event **e,
+			  	     struct ResourceInfo **res_info_ptr,
+				     SaHpiEntityPathT *ep_root, 
+				     guint pm_index);
+
+SaErrorT snmp_bc_construct_sm_res(struct oh_event **e,
+			  	  struct ResourceInfo **res_info_ptr,
+				  SaHpiEntityPathT *ep_root, 
+				  guint sm_index);
+				  
+SaErrorT snmp_bc_construct_mm_res(struct oh_event **e,
+			  	  struct ResourceInfo **res_info_ptr,
+				  SaHpiEntityPathT *ep_root, 
+				  guint mm_index);
+					       				  		       
 
 #endif
 
