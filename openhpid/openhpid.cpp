26ef9decbe2d09554ecd8845fdd33f934693efd1
djudkovi <djudkovi@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2006-09-19 15:09:19 +0000

    [ 1533338 ] Injector API without using linux queues

--- openhpid/openhpid.cpp
+++ openhpid/openhpid.cpp
@@ -713,7 +713,7 @@ static tResult HandleMsg(psstrmsock thrdinst, char *data, GHashTable **ht,
                 
                         memset( &rdr, 0, sizeof( SaHpiRdrT ) );
                         memset( &rpt_entry, 0, sizeof( SaHpiRptEntryT ) );
-			memset( &event_log_entry, 0, sizeof( SaHpiEventLogEntryT ) );
+                        memset( &event_log_entry, 0, sizeof( SaHpiEventLogEntryT ) );
                 
                         if ( HpiDemarshalRequest3( request_mFlags & dMhEndianBit,
                                                         hm, pReq, &session_id, &resource_id,
@@ -2184,6 +2184,33 @@ static tResult HandleMsg(psstrmsock thrdinst, char *data, GHashTable **ht,
                         ret = oHpiGlobalParamSet(&param);
                 
                         thrdinst->header.m_len = HpiMarshalReply0( hm, pReq, &ret );
+                        result = eResultClose;
+                }
+                break;
+
+                case eFoHpiInjectEvent: {
+                        oHpiHandlerIdT  id;
+                        SaHpiEventT     event;
+                        SaHpiRptEntryT  rpte;
+                        SaHpiRdrT       rdrs;
+
+                
+                        PVERBOSE1("%p Processing oHpiInjectEvent.\n", thrdid);
+                
+                        if ( HpiDemarshalRequest4( request_mFlags & dMhEndianBit,
+                                                   hm, 
+                                                   pReq, 
+                                                   &id, 
+                                                   &event,
+                                                   &rpte, 
+                                                   &rdrs ) < 0 )
+                                return eResultError;
+
+
+                        ret = oHpiInjectEvent(id, &event, &rpte, NULL);
+
+                        thrdinst->header.m_len = HpiMarshalReply0( hm, pReq, &ret );
+
                         result = eResultClose;
                 }
                 break;
