308b10983e892cb82d2befe952a2b89f3213f3c0
renierm <renierm@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2006-12-01 22:14:03 +0000

    #1607093: compile error in ipmidirect, #1607115: ohpi tests using UNSPECIFIED_DOMAIN_ID, #1607119: supressing superfluous output from hpiinjector test, #1607128: Including sys/stat.h

--- openhpi/branches/2.7.x/src/t/ohpi/ohpi_034.c
+++ openhpi/branches/2.7.x/src/t/ohpi/ohpi_034.c
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
         
         setenv("OPENHPI_CONF","./noconfig", 1);
         
-        if (saHpiSessionOpen(1, &sid, NULL))
+        if (saHpiSessionOpen(SAHPI_UNSPECIFIED_DOMAIN_ID, &sid, NULL))
                 return -1;
                 
         if (oHpiPluginLoad("libsimulator"))
