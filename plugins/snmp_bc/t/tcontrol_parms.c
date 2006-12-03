5fcba4b0aa2e41ad99e197ea76a9928669f9bbed
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2006-12-03 22:02:47 +0000

    Convert snmp_bc simulator testcases to snmpwalk-driven sim, feature 1606780

--- plugins/snmp_bc/t/tcontrol_parms.c
+++ plugins/snmp_bc/t/tcontrol_parms.c
@@ -1,6 +1,6 @@
 /* -*- linux-c -*-
  * 
- * (C) Copyright IBM Corp. 2004, 2005
+ * (C) Copyright IBM Corp. 2004, 2005, 2006
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -31,7 +31,14 @@ int main(int argc, char **argv)
 	SaErrorT expected_err;
 
         SaHpiSessionIdT sessionid;
+	struct oh_handler_state *handle;
 	 
+// 
+        SaHpiDomainIdT did;
+        struct oh_handler *h = NULL;
+        struct oh_domain *d = NULL;
+        unsigned int *hid = NULL;
+//			 
 	/* ************************	 	 
 	 * Find a resource with Control type rdr
 	 * ***********************/
@@ -54,11 +61,14 @@ int main(int argc, char **argv)
 
 	id = rptentry.ResourceId;
 	act = SAHPI_RESTORE_PARM;
-	
-#if 0			
 
-	struct oh_handler_state handle;
-	memset(&handle, 0, sizeof(struct oh_handler_state));
+// -----------
+		did = oh_get_session_domain(sessionid);
+		d = oh_get_domain(did);	
+                hid = oh_get_resource_data(&(d->rpt), id);
+                h = oh_get_handler(*hid);
+// ----------
+//	memset(&handle, 0, sizeof(struct oh_handler_state));
 	/************************** 
 	 * Test 1: Invalid Control Action
 	 **************************/
@@ -66,28 +76,29 @@ int main(int argc, char **argv)
 	expected_err = SA_ERR_HPI_INVALID_PARAMS;
 	act = 0xFF;
 																																														
-	err = snmp_bc_control_parm(&handle, id, act);
+	err = snmp_bc_control_parm(h->hnd, id, act);
 	checkstatus(err, expected_err, testfail);
 	
 	/************************** 
 	 * Test 2: Invalid ResourceId
 	 **************************/
+	act = SAHPI_DEFAULT_PARM;
 	expected_err = SA_ERR_HPI_INVALID_RESOURCE;
 
-	err = snmp_bc_control_parm(&handle, 5000, act);
+	err = snmp_bc_control_parm(h->hnd, 5000, act);
 	checkstatus(err, expected_err, testfail);
 
 
 	/************************** 
 	 * Test 3: Resource configuration saving not supported
 	 *************************/
+	handle = (struct oh_handler_state *) h->hnd;
 	rptentry.ResourceCapabilities |= SAHPI_CAPABILITY_CONFIGURATION;  
 	oh_add_resource(handle->rptcache, &rptentry, NULL, 0);
 	expected_err = SA_ERR_HPI_INTERNAL_ERROR;
 
-	err = snmp_bc_control_parm(&handle, id, act);
+	err = snmp_bc_control_parm(h->hnd, id, act);
 	checkstatus(err, expected_err, testfail);
-#endif
 
 	/************************** 
 	 * Test 4: Normal Path
@@ -107,3 +118,4 @@ int main(int argc, char **argv)
 }
 
 #include <tsetup.c>
+
