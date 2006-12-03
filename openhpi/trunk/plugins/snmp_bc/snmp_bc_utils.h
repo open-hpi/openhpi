e8c1021c74ee747c86fb6749bc66c90b29bb0afb
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2006-12-03 21:59:40 +0000

    Support New BladeCenter Resources, Feature 1598595

--- openhpi/trunk/plugins/snmp_bc/snmp_bc_utils.h
+++ openhpi/trunk/plugins/snmp_bc/snmp_bc_utils.h
@@ -26,6 +26,10 @@ SaErrorT snmp_bc_get_guid(struct snmp_bc_hnd *custom_handle,
 SaErrorT snmp_bc_extract_slot_ep(SaHpiEntityPathT *resource_ep,
 				 SaHpiEntityPathT *slot_ep);
 				 
+SaErrorT snmp_bc_extend_ep(struct oh_event *e,
+			   guint resource_index, 
+			   gchar *interposer_intalled_mask); 
+				 
 struct oh_event *snmp_bc_alloc_oh_event(void);
 
 void snmp_bc_free_oh_event(struct oh_event *e);
@@ -33,6 +37,6 @@ void snmp_bc_free_oh_event(struct oh_event *e);
 SaErrorT snmp_bc_set_resource_add_oh_event(struct oh_event *e, 
 					struct ResourceInfo *res_info_ptr);
 
-SaErrorT snmp_bc_copy_oh_event(struct oh_event *new_event, struct oh_event *old_event);							  			  
+SaErrorT snmp_bc_copy_oh_event(struct oh_event *new_event, struct oh_event *old_event);						  			  
 
 #endif
