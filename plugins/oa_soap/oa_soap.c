20d2a73b5490f7940eaca8b243d69c711e235ac3
sutula <sutula@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2008-04-30 21:42:13 +0000

    [ 1951470 ] Re-Discovery hangs on inserting OA which was removed earlier

--- plugins/oa_soap/oa_soap.c
+++ plugins/oa_soap/oa_soap.c
@@ -61,7 +61,7 @@
 
 /* For maintaining the patch versions */
 static char const rcsid[] __attribute__ ((used)) =
-        "$Version: oa_soap plugin for openhpi-2.11.2, patch level 7. "
+        "$Version: oa_soap plugin for openhpi-2.11.2, patch level 8. "
         "Created on Apr 28, 2008 $";
 
 /**
