77a2dcbe9e6e5411f0e8d5b22f8505a4ba6d6077
pdphan <pdphan@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2008-07-18 03:38:52 +0000

    Handle return code to eliminate compiler warning, bug 1963765

--- clients/hpionIBMblade.c
+++ clients/hpionIBMblade.c
@@ -441,9 +441,10 @@ static void show_sens(char *S)
 static int get_number(char *mes, int *res)
 {
 	char	buf[READ_BUF_SIZE];
+	char    *ret;
 
 	printf("%s", mes);
-	fgets(buf, READ_BUF_SIZE, stdin);
+	ret = fgets(buf, READ_BUF_SIZE, stdin);
 	return (sscanf(buf, "%d", res));
 }
 
@@ -507,8 +508,7 @@ static void mod_sen(void)
 		Rdr->is_value = 1;
 	};
 	printf("threshold type (lc, la, li, uc, ua, ui, ph, nh): ");
-	fgets(buf, READ_BUF_SIZE, stdin);
-	S = buf;
+	S = fgets(buf, READ_BUF_SIZE, stdin);
 	while (*S == ' ') S++;
 	if (strlen(S) < 2) {
 		printf("ERROR: invalid threshold type: %s\n", S);
@@ -530,7 +530,7 @@ static void mod_sen(void)
 	};
 	
 	printf("new value: ");
-	fgets(buf, READ_BUF_SIZE, stdin);
+	S = fgets(buf, READ_BUF_SIZE, stdin);
 	i = sscanf(buf, "%f", &f);
 	if (i == 0) {
 		printf("ERROR: no value\n");
@@ -567,8 +567,7 @@ static void mod_sen(void)
 	printf("\n  Nem threshold:\n");
 	ShowThres(&thres);
 	printf("Is it correct (yes, no)?:");
-	fgets(buf, READ_BUF_SIZE, stdin);
-	S = buf;
+	S = fgets(buf, READ_BUF_SIZE, stdin);
 	while (*S == ' ') S++;
 	if (strncmp(S, "yes", 3) != 0)
 		return;
