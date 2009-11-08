4e6b1d40c59885758a1c0576a025f731271ea108
ricwhite <ricwhite@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2009-11-08 21:38:40 +0000

    Merging trunk changes since the 2.14.0 release into the 2.14.x branch for 2.14.1 testing. Merge revision parameter was -r6999:7024.

--- clients/hpitop.c
+++ clients/hpitop.c
@@ -219,6 +219,13 @@ SaErrorT list_resources(SaHpiSessionIdT sessionid,SaHpiResourceIdT resourceid)
 
 				if (rvRdrGet == SA_OK)
 				{
+					// Add zero terminator to RDR Id String
+					SaHpiUint32T last = rdr.IdString.DataLength;
+					if ( last >= SAHPI_MAX_TEXT_BUFFER_LENGTH ) {
+						last = SAHPI_MAX_TEXT_BUFFER_LENGTH - 1;
+					}
+					rdr.IdString.Data[last] = '\0';
+
 					if (f_overview) list_rdr(sessionid, &rptentry, &rdr, l_resourceid);
 					if (f_inv) show_inv(sessionid, &rptentry, &rdr, l_resourceid); 
 					if (f_sensor) show_sens(sessionid, &rptentry, &rdr, l_resourceid); 
