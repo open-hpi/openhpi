2f71e5f42b596caad959ca7436a6997ea549870b
avpak <avpak@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2010-12-18 21:48:30 +0000

    More on feature request #3031797

---
+++ openhpid/openhpid-win32.cpp
@@ -0,0 +1,39 @@
+/*      -*- linux-c -*-
+ *
+ * (C) Copyright Pigeon Point Systems. 2010
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Author(s):
+ *     Anton Pak <anton.pak@pigeonpoint.com>
+ *
+ */
+
+#include <stdio.h>
+
+#include <glib.h>
+
+#include <oHpi.h>
+
+#include <oh_init.h>
+
+#include "server.h"
+
+
+/*--------------------------------------------------------------------*/
+/* Function: main                                                     */
+/*--------------------------------------------------------------------*/
+
+int main(int argc, char *argv[])
+{
+    // TODO implement
+    printf( "Hello World\n" );
+
+    return 0;
+}
+
