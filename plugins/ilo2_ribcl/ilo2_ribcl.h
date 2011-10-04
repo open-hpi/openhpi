907eaba49926ac8f68665d809941c0b82c16cd0f
elangoc <elangoc@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2011-10-04 21:29:25 +0000

    This patch fixes bug ID: 3416584 - ilo2_ribcl: FreeBSD compilation issue

--- plugins/ilo2_ribcl/ilo2_ribcl.h
+++ plugins/ilo2_ribcl/ilo2_ribcl.h
@@ -44,7 +44,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <bits/local_lim.h>
 
 #include <SaHpi.h>
 #include <oh_utils.h>
@@ -89,6 +88,7 @@
  * ILO2 RIBCL Hostname Length defines
  */
 #define ILO2_HOST_NAME_MAX_LEN 256
+#define HOST_NAME_MAX 255
 /* A valid IP addr will have a min. len of 7 4 digts + 3 decimal points */
 #define ILO2_HOST_NAME_MIN_LEN 7
 
